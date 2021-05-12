1aecf5bd81c6de54f18898379e170710c4fc4e60
Brindy <brindy@duckduckgo.com>
2021-05-12 01:36:28 +0100

    exclude unwanted resources to remove warnings

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
         .package(name: "GRDB", url: "https://github.com/duckduckgo/GRDB.swift.git", .revision("e5714d4b6ee1651d2271b04ae85aaf5a327fe70a")),
     ],
     targets: [
-        .target(
+        Target.target(
             name: "BrowserServicesKit",
             dependencies: [],
-            // ideally exclude `Resources/duckduckgo-autofill` explicitly, but doing so prevents Bundle.module being generated
             resources: [
                 .process("Resources/duckduckgo-autofill/dist/autofill.js")
-            ]),
+            ]).excluding(filesRecursingDirectoryAt: "Resources/duckduckgo-autofill/",
+                         except: ["dist/autofill.js"]),
         .target(
             name: "SecureVault",
             dependencies: [
@@ -42,3 +42,52 @@ let package = Package(
             ])
     ]
 )
+
+extension Target {
+
+    func excluding(filesRecursingDirectoryAt exclusionPath: String,
+                   except exceptions: [String]) -> Target {
+        assert(sources == nil, "Custom sources location not supported.")
+
+        func allFiles(recursingDirectory dir: URL) -> [URL] {
+            let fm = FileManager.default
+            let files = (try? fm.contentsOfDirectory(at: dir,
+                                                     includingPropertiesForKeys: nil,
+                                                     options: [ .skipsHiddenFiles ])) ?? []
+            return files.map {
+                $0.hasDirectoryPath ? allFiles(recursingDirectory: $0) : [$0]
+            }.flatMap {
+                $0
+            }
+        }
+
+        var sourcesPath = URL(fileURLWithPath: #file)
+        sourcesPath.deleteLastPathComponent()
+        sourcesPath.appendPathComponent("Sources")
+        sourcesPath.appendPathComponent(name)
+
+        let exclusionPath = sourcesPath.appendingPathComponent(exclusionPath)
+        let exceptionUrls = exceptions.map {
+            URL(fileURLWithPath: $0, relativeTo: exclusionPath)
+        }
+
+        let exclusions = allFiles(recursingDirectory: exclusionPath).filter({
+            !exceptionUrls.contains($0)
+        }).map {
+            String($0.path.dropFirst(sourcesPath.path.count + 1))
+        }
+
+        return .target(name: name,
+                       dependencies: dependencies,
+                       path: path,
+                       exclude: exclude + exclusions,
+                       sources: sources,
+                       resources: resources,
+                       publicHeadersPath: publicHeadersPath,
+                       cSettings: cSettings,
+                       cxxSettings: cxxSettings,
+                       swiftSettings: swiftSettings,
+                       linkerSettings: linkerSettings)
+    }
+
+}
