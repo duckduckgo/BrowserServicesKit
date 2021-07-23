e6f25a5d09e91b232fd734e70775f7cab2218696
Sam Symons <sam@samsymons.com>
2021-07-23 07:16:08 -0700

    Test whether IF EXISTS works.

--- Sources/BrowserServicesKit/SecureVault/SecureVaultDatabaseProvider.swift
+++ Sources/BrowserServicesKit/SecureVault/SecureVaultDatabaseProvider.swift
@@ -253,7 +253,17 @@ extension DefaultDatabaseProvider {
         try database.drop(table: Account.databaseTableName + "Old")
         try database.drop(table: Credentials.databaseTableName + "Old")
 
-        try database.drop(index: Account.databaseTableName + "_unique")
+        try database.execute(sql: "DROP INDEX IF EXISTS \((Account.databaseTableName + "_unique").quotedDatabaseIdentifier)")
+
+//        do {
+//            try database.drop(index: Account.databaseTableName + "_unique")
+//        } catch {
+//            if let databaseError = error as? GRDB.DatabaseError, databaseError.extendedResultCode == .SQLITE_ERROR {
+//                // No-op in the case where the database table doesn't already exist
+//            } else {
+//                throw error
+//            }
+//        }
 
         // ifNotExists: false will throw an error if this exists already, which is ok as this shouldn't get called more than once
         try database.create(index: Account.databaseTableName + "_unique",
