563a2c02f33d77af4f673b99d98731cdbc110273
Chris Brind <brindy@duckduckgo.com>
2022-04-20 18:09:14 +0100

    add create account function

--- Sources/DDGSyncAuth/DDGSyncAuth.c
+++ Sources/DDGSyncAuth/DDGSyncAuth.c
@@ -1,33 +1,91 @@
 
-#include "DDGSyncAuth.h"
-#include "sodium.h"
 #include <string.h>
 
-DDGSyncAuthResult ddgSyncCreateKeyAndPasswordHash(
-    char *userId,                                      // IN - use a UUID
-    char *password,                                    // IN - use a UUID
-    char primaryKey[DDGSYNCAUTH_PRIMARY_KEY_SIZE],    // OUT - store this securely
-    char passwordHash[DDGSYNCAUTH_HASH_SIZE]           // OUT - Use in API calls but don't store it
-) {
+#include "DDGSyncAuth.h"
+#include "sodium.h"
 
-    if (NULL == userId || NULL == password) {
-        return DDGSYNCAUTH_INVALID_ARGS;
+// Seems mad that you still need to define this?!
+#define min(x, y) (x < y) ? x : y
+
+// Contexts must be 8 characters long but are otherwise arbitrary
+#define DDGSYNC_STRETCHED_PRIMARY_KEY_CONTEXT "Stretchy"
+#define DDGSYNC_PASSWORD_HASH_CONTEXT "Password"
+
+// Other private macros
+#define DDGSYNCAUTH_STRETCHED_PRIMARY_KEY_SIZE DDGSYNCAUTH_PRIMARY_KEY_SIZE * 2
+
+enum DDGSyncAuthSubkeyIds : int {
+
+    DDGSyncAuthPasswordHashSubkey = 1,
+    DDGSyncAuthStretchedPrimaryKeySubkey,
+
+};
+
+DDGSyncAuthResult ddgSyncCreateAccount(
+    unsigned char primaryKey[DDGSYNCAUTH_PRIMARY_KEY_SIZE],
+    unsigned char protectedSymmetricKey[DDGSYNCAUTH_PROTECTED_SYMMETRIC_KEY_SIZE],
+    unsigned char passwordHash[DDGSYNCAUTH_HASH_SIZE],
+    const char *userId,
+    const char *password) {
+
+    // Define VARS
+    unsigned char salt[crypto_pwhash_SALTBYTES];
+    unsigned char stretchedPrimaryKey[DDGSYNCAUTH_STRETCHED_PRIMARY_KEY_SIZE];
+    unsigned char secretKey[crypto_secretbox_KEYBYTES];
+    unsigned char nonceBytes[crypto_secretbox_NONCEBYTES];
+
+    // Validate inputs
+    if (NULL == userId) {
+        return DDGSYNCAUTH_INVALID_USERID;
     }
 
-    return DDGSYNCAUTH_OK;
-}
+    if (NULL == password) {
+        return DDGSYNCAUTH_INVALID_PASSWORD;
+    }
 
-DDGSyncAuthResult ddgSyncCreatePasswordHash(
-    const char primaryKey[DDGSYNCAUTH_PRIMARY_KEY_SIZE],   // IN
-    char passwordHash[DDGSYNCAUTH_HASH_SIZE]                // OUT - Use in API calls but don't store it
-) {
+    // Prepare salt
+    memset(salt, 0, crypto_pwhash_SALTBYTES);
+    memcpy(salt, userId, min(crypto_pwhash_SALTBYTES, strlen(userId)));
 
-    if (0 != crypto_pwhash_str(passwordHash,
-                                primaryKey,
-                                strlen(primaryKey),
-                                crypto_pwhash_OPSLIMIT_SENSITIVE,
-                                crypto_pwhash_MEMLIMIT_INTERACTIVE)) {
-        return DDGSYNCAUTH_UNKNOWN_ERROR;
+    // Create hash and keys
+
+    if (0 != crypto_pwhash(primaryKey,
+                           DDGSYNCAUTH_PRIMARY_KEY_SIZE,
+                           password,
+                           strlen(password),
+                           salt,
+                           crypto_pwhash_OPSLIMIT_INTERACTIVE,
+                           crypto_pwhash_MEMLIMIT_INTERACTIVE,
+                           crypto_pwhash_ALG_DEFAULT)) {
+
+        return DDGSYNCAUTH_CREATE_PRIMARY_KEY_FAILED;
+    }
+
+    if (0 != crypto_kdf_derive_from_key(passwordHash,
+                                        DDGSYNCAUTH_HASH_SIZE,
+                                        DDGSyncAuthPasswordHashSubkey,
+                                        DDGSYNC_PASSWORD_HASH_CONTEXT,
+                                        primaryKey)) {
+        return DDGSYNCAUTH_CREATE_PASSWORD_HASH_FAILED;
+    }
+
+    if (0 != crypto_kdf_derive_from_key(stretchedPrimaryKey,
+                                        DDGSYNCAUTH_STRETCHED_PRIMARY_KEY_SIZE,
+                                        DDGSyncAuthStretchedPrimaryKeySubkey,
+                                        DDGSYNC_STRETCHED_PRIMARY_KEY_CONTEXT,
+                                        primaryKey)) {
+        return DDGSYNCAUTH_CREATE_STRETCHED_PRIMARY_KEY_FAILED;
+    }
+
+    randombytes_buf(secretKey, crypto_secretbox_KEYBYTES);
+    randombytes_buf(nonceBytes, crypto_secretbox_NONCEBYTES);
+
+    if (0 != crypto_secretbox_easy(protectedSymmetricKey,
+                                   stretchedPrimaryKey,
+                                   DDGSYNCAUTH_STRETCHED_PRIMARY_KEY_SIZE,
+                                   nonceBytes,
+                                   secretKey)) {
+        return DDGSYNCAUTH_CREATE_PROTECTED_SECRET_KEY_FAILED;
     }
 
     return DDGSYNCAUTH_OK;
--- Sources/DDGSyncAuth/include/DDGSyncAuth.h
+++ Sources/DDGSyncAuth/include/DDGSyncAuth.h
@@ -1,38 +1,44 @@
 #import "sodium/crypto_pwhash.h"
-#import "sodium/crypto_kdf.h"
+#import "sodium/crypto_box.h"
+#import "sodium/crypto_secretbox.h"
 
 #ifndef DDGSyncAuth_h
 #define DDGSyncAuth_h
 
-#define DDGSYNCAUTH_HASH_SIZE crypto_pwhash_STRBYTES
-#define DDGSYNCAUTH_PRIMARY_KEY_SIZE crypto_kdf_KEYBYTES
+#define DDGSYNCAUTH_HASH_SIZE 32
 
-#define DDGSYNCAUTH_CIPHERTEXT_LEN (crypto_secretbox_MACBYTES + MESSAGE_LEN)
-
-#define DDGSYNC_KEY_CONTEXT "DuckSync" // must be 8 characters long but is otherwise arbitrary
+// TODO use the constants, but get them compiled here
+#define DDGSYNCAUTH_PRIMARY_KEY_SIZE 32 // crypto_box_SEEDBYTES
+#define DDGSYNCAUTH_PROTECTED_SYMMETRIC_KEY_SIZE 128 // crypto_secretbox_MACBYTES + DDGSYNCAUTH_PRIMARY_KEY_SIZE
 
 typedef enum : int {
     DDGSYNCAUTH_OK,
     DDGSYNCAUTH_UNKNOWN_ERROR,
-    DDGSYNCAUTH_INVALID_ARGS,
+    DDGSYNCAUTH_INVALID_USERID,
+    DDGSYNCAUTH_INVALID_PASSWORD,
+    DDGSYNCAUTH_CREATE_PRIMARY_KEY_FAILED,
+    DDGSYNCAUTH_CREATE_PASSWORD_HASH_FAILED,
+    DDGSYNCAUTH_CREATE_STRETCHED_PRIMARY_KEY_FAILED,
+    DDGSYNCAUTH_CREATE_PROTECTED_SECRET_KEY_FAILED,
 } DDGSyncAuthResult;
 
-/*
- * Use when creating a new account.
+/**
+ * Used to create data needed to create an account.  Once the server returns a JWT, then store primary and secret key.
+ *
+ * @param primaryKey OUT - store this.  In combination with user id, this is the recovery key.
+ * @param protectedSymmetricKey OUT - store this. Used to encrypt and decrypt e2e data.
+ * @param passwordHash OUT - do not store this.  Send to /signup endpoint.
+ * @param userId IN
+ * @param password IN
  */
-extern DDGSyncAuthResult ddgSyncCreateKeyAndPasswordHash(
-    char *userId,                                       // IN - use a UUID
-    char *password,                                     // IN - use a UUID
-    char primaryKey[DDGSYNCAUTH_PRIMARY_KEY_SIZE],                   // OUT - store this securely
-    char passwordHash[DDGSYNCAUTH_HASH_SIZE]           // OUT - Use in API calls but don't store it
+extern DDGSyncAuthResult ddgSyncCreateAccount(
+    unsigned char primaryKey[DDGSYNCAUTH_PRIMARY_KEY_SIZE],
+    unsigned char protectedSymmetricKey[DDGSYNCAUTH_PROTECTED_SYMMETRIC_KEY_SIZE],
+    unsigned char passwordHash[DDGSYNCAUTH_HASH_SIZE],
+    const char *userId,
+    const char *password
 );
 
-/*
- * Use when you need to make an API call.
- */
-extern DDGSyncAuthResult ddgSyncCreatePasswordHash(
-    const char primaryKey[DDGSYNCAUTH_PRIMARY_KEY_SIZE],                      // IN
-    char passwordHash[DDGSYNCAUTH_HASH_SIZE]           // OUT - Use in API calls but don't store it
-);
+// TODO get secret key from protected symmetric key?
 
 #endif /* DDGSyncAuth_h */
