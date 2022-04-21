47edd8c935d133a1bf24785d228e858eef1effc0
Chris Brind <brindy@duckduckgo.com>
2022-04-21 17:17:16 +0100

    improve tests to show how to use the c-function

--- Tests/DDGSyncAuthTests/DDGSyncAuthTests.swift
+++ Tests/DDGSyncAuthTests/DDGSyncAuthTests.swift
@@ -5,26 +5,21 @@ import DDGSyncAuth
 class DDGSyncAuthTests: XCTestCase {
 
     func testCreateAccount() {
-        let primaryKey = UnsafeMutablePointer<CChar>.allocate(capacity: Int(DDGSYNCAUTH_PRIMARY_KEY_SIZE))
-        let protectedSymmetricKey = UnsafeMutablePointer<CChar>.allocate(capacity: Int(DDGSYNCAUTH_PROTECTED_SYMMETRIC_KEY_SIZE))
-        let passwordHash = UnsafeMutablePointer<CChar>.allocate(capacity: Int(DDGSYNCAUTH_HASH_SIZE))
+        var primaryKey = [UInt8](repeating: 0, count: Int(DDGSYNCAUTH_PRIMARY_KEY_SIZE))
+        var protectedSymmetricKey = [UInt8](repeating: 0, count: Int(DDGSYNCAUTH_PROTECTED_SYMMETRIC_KEY_SIZE))
+        var passwordHash = [UInt8](repeating: 0, count: Int(DDGSYNCAUTH_HASH_SIZE))
 
-        defer {
-            primaryKey.deallocate()
-            protectedSymmetricKey.deallocate()
-            passwordHash.deallocate()
+        XCTAssertEqual(DDGSYNCAUTH_OK, ddgSyncCreateAccount(&primaryKey, &protectedSymmetricKey, &passwordHash, "UserID", "Password"))
+
+        assertValidKey(primaryKey)
+        assertValidKey(protectedSymmetricKey)
+        assertValidKey(passwordHash)
+    }
+
+    func assertValidKey(_ key: [UInt8], file: StaticString = #file, line: UInt = #line) {
+        for i in 0 ..< key.count {
+            XCTAssertNotEqual(0, key[i], "element \(i) is NULL", file: file, line: line)
         }
-
-        XCTAssertTrue(String(cString: primaryKey).isEmpty)
-        XCTAssertTrue(String(cString: protectedSymmetricKey).isEmpty)
-        XCTAssertTrue(String(cString: passwordHash).isEmpty)
-
-        XCTAssertEqual(DDGSYNCAUTH_OK, ddgSyncCreateAccount(primaryKey, protectedSymmetricKey, passwordHash, "UserID", "Password"))
-
-        XCTAssertFalse(String(cString: primaryKey).isEmpty)
-        XCTAssertFalse(String(cString: protectedSymmetricKey).isEmpty)
-        XCTAssertFalse(String(cString: passwordHash).isEmpty)
-
     }
 
 }
