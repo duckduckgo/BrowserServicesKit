c6a35e50ead6959952abae95c72d1600f676042d
Brad Slayter <slayterdevelopment@gmail.com>
2022-04-29 08:23:46 -0500

    Update TrackerRadarKit

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "GRDB", url: "https://github.com/duckduckgo/GRDB.swift.git", .exact("1.1.0")),
-        .package(url: "https://github.com/duckduckgo/TrackerRadarKit", .exact("1.0.3"))
+        .package(url: "https://github.com/duckduckgo/TrackerRadarKit", .branch("brad/cname-load-types"))
     ],
     targets: [
         
