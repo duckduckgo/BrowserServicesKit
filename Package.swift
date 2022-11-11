9a894c64e8ded43e988ba0727810c4c53fabb6d9
Alexey Martemyanov <mallexxx@gmail.com>
2022-11-11 09:38:45 +0600

    Unsafe std=c++11 -> cxxLanguageStandard: .cxx11

--- Package.swift
+++ Package.swift
@@ -48,9 +48,6 @@ let package = Package(
             name: "BloomFilter",
             resources: [
                 .process("CMakeLists.txt")
-            ],
-            cxxSettings: [
-                .unsafeFlags(["-std=c++11"])
             ]),
         .target(
             name: "Common",
@@ -81,5 +78,6 @@ let package = Package(
                 "Common"
             ]
         )
-    ]
+    ],
+    cxxLanguageStandard: .cxx11
 )
