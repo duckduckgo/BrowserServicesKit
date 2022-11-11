8872fdf232712a5f69354df8e8b0c5554e1cd64f
Chris Brind <brindy@duckduckgo.com>
2022-11-11 21:31:26 +0000

    add support for pixel based crash collection

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
     ],
     products: [
         .library(name: "BrowserServicesKit", targets: ["BrowserServicesKit"]),
-        .library(name: "Common", targets: ["Common"])
+        .library(name: "Common", targets: ["Common"]),
+        .library(name: "Crashes", targets: ["Crashes"])
     ],
     dependencies: [
         .package(name: "Autofill", url: "https://github.com/duckduckgo/duckduckgo-autofill.git", .exact("5.2.0")),
@@ -30,7 +31,8 @@ let package = Package(
                 "GRDB",
                 "TrackerRadarKit",
                 "BloomFilterWrapper",
-                "Common"
+                "Common",
+                "Crashes"
             ],
             resources: [
                 .process("ContentBlocking/UserScripts/contentblockerrules.js"),
@@ -44,6 +46,9 @@ let package = Package(
             dependencies: [
                 "BloomFilter"
             ]),
+        .target(
+            name: "Crashes",
+            dependencies: [ ]),
         .target(
             name: "BloomFilter",
             resources: [
