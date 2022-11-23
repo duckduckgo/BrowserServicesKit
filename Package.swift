084310c5076cb5978bca37db2d8b69547d81c8a2
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2022-11-23 16:55:17 +0100

    Fix bitwarden tooltip icon
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(name: "Common", targets: ["Common"])
     ],
     dependencies: [
-        .package(name: "Autofill", url: "https://github.com/duckduckgo/duckduckgo-autofill.git", .revision("0e1ad5724514289a8465cd7d4fbe7b5683e72580")),
+        .package(name: "Autofill", url: "https://github.com/duckduckgo/duckduckgo-autofill.git", .revision("b2c8ce84c19ae61ef2f32305687149a2471b056c")),
         .package(name: "GRDB", url: "https://github.com/duckduckgo/GRDB.swift.git", .exact("1.2.1")),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", .exact("1.1.1")),
         .package(name: "Punycode", url: "https://github.com/gumob/PunycodeSwift.git", .exact("2.1.0")),
