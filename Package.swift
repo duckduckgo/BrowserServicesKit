bfd98ef1f2c930afaad42b658847aea0f362614b
Sam Symons <sam@samsymons.com>
2022-12-01 17:55:30 -0800

    Update the Autofill reference.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .library(name: "PrivacyDashboard", targets: ["PrivacyDashboard"])
     ],
     dependencies: [
-        .package(name: "Autofill", url: "https://github.com/duckduckgo/duckduckgo-autofill.git", .branch("ema/send-js-pixels")),
+        .package(name: "Autofill", url: "https://github.com/duckduckgo/duckduckgo-autofill.git", .revision("d830d6e047d7f43504319635d4ca90cc935244d8")),
         .package(name: "GRDB", url: "https://github.com/duckduckgo/GRDB.swift.git", .exact("1.2.1")),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", .exact("1.1.1")),
         .package(name: "Punycode", url: "https://github.com/gumob/PunycodeSwift.git", .exact("2.1.0")),
