4d4604d2de4647852b130bb98f958e517f7706a3
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-01-18 18:27:25 +0100

    Update GRDB to 2.0.0 (upstream 6.6.0, SQLCipher 4.5.3)
    Task/Issue URL: https://app.asana.com/0/1200194497630846/1203762149400705/f

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Autofill", url: "https://github.com/duckduckgo/duckduckgo-autofill.git", .exact("6.1.1")),
-        .package(name: "GRDB", url: "https://github.com/duckduckgo/GRDB.swift.git", .exact("1.2.1")),
+        .package(name: "GRDB", url: "https://github.com/duckduckgo/GRDB.swift.git", .exact("2.0.0")),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", .exact("1.1.1")),
         .package(name: "Punycode", url: "https://github.com/gumob/PunycodeSwift.git", .exact("2.1.0")),
         .package(url: "https://github.com/duckduckgo/content-scope-scripts", .exact("3.4.1")),
