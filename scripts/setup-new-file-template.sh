bc59861d1e4f257c4a2ced3952434d61a86fbf02
Jacek Łyp <jlyp@duckduckgo.com>
2023-02-17 14:48:17 +0100

    Use no-dereference option which should prevent the creation of the duplicate symbolic link at the source

--- scripts/setup-new-file-template.sh
+++ scripts/setup-new-file-template.sh
@@ -7,20 +7,8 @@ script_dir=$(dirname "$(readlink -f "$0")")
 source="$script_dir/assets/Swift File For Package.xctemplate"
 destination="/Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File Templates/MultiPlatform/Source/Swift File For Package.xctemplate"
 
-# Store the current working directory
-original_dir=$(pwd)
-
-# Change the working directory to the assets directory to ensure that the symbolic
-# link is created with the correct relative paths and only in the intended destination directory.
-# Without this, the ln command would create the symbolic link in the current working directory 
-# as well as the destination directory.
-(
-  cd "$script_dir/assets" || exit
-  ln -sF "$source" "$destination"
-)
-
-# Restore the original working directory
-cd "$original_dir" || exit
+# Create a symbolic link
+ln -sFn "$source" "$destination"
 
 # Check if the symlink was created successfully
 if [[ ! -L "$destination" ]]; then
