86a7a3a68f53a37aa292ff3193b2dd587f943527
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-02-24 20:27:17 +0100

    Revert to GRDB.swift 2.0.0 (upstream 6.6.0) (#244)
    * Revert "Update Package.resolved"
    
    This reverts commit 11980e56e60ee894ed13386a85c4a9af2dee5430.
    
    * Revert "Bump GRDB.swift to 2.1.0 (#239)"
    
    This reverts commit 451d05ad1c14c9dfd83f57e715bc6f21ad7eb080.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Autofill", url: "https://github.com/duckduckgo/duckduckgo-autofill.git", .exact("6.3.0")),
-        .package(name: "GRDB", url: "https://github.com/duckduckgo/GRDB.swift.git", .exact("2.1.0")),
+        .package(name: "GRDB", url: "https://github.com/duckduckgo/GRDB.swift.git", .exact("2.0.0")),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", .exact("1.2.1")),
         .package(name: "Punycode", url: "https://github.com/gumob/PunycodeSwift.git", .exact("2.1.0")),
         .package(url: "https://github.com/duckduckgo/content-scope-scripts", .exact("3.4.1")),
