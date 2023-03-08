774647c2f4d91f5e0b4aad11132cbae3c67e5c7c
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-03-08 14:45:43 +0100

    Use sync_crypto 0.0.1

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .package(name: "Autofill", url: "https://github.com/duckduckgo/duckduckgo-autofill.git", .exact("6.3.0")),
         .package(name: "GRDB", url: "https://github.com/duckduckgo/GRDB.swift.git", .exact("2.0.0")),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", .exact("1.2.1")),
-        .package(url: "https://github.com/duckduckgo/sync_crypto", .branch("dominik/spm")),
+        .package(url: "https://github.com/duckduckgo/sync_crypto", .exact("0.0.1")),
         .package(name: "Punycode", url: "https://github.com/gumob/PunycodeSwift.git", .exact("2.1.0")),
         .package(url: "https://github.com/duckduckgo/content-scope-scripts", .exact("3.4.1")),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", .exact("1.4.0")),
