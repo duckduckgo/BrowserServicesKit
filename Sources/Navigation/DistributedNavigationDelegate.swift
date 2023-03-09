c38de5453fa8892259075d9e7af8b43c7a692b44
Alexey Martemyanov <mallexxx@gmail.com>
2023-03-09 18:08:05 +0600

    fix assertion for popup about:blank navigations without WKNavigation (#247)
    * fix assertion for popup about:blank navigations without WKNavigation
    
    * fix popup navigating to about: scheme

--- Sources/Navigation/DistributedNavigationDelegate.swift
+++ Sources/Navigation/DistributedNavigationDelegate.swift
@@ -413,13 +413,19 @@ extension DistributedNavigationDelegate: WKNavigationDelegate {
     @MainActor
     public func webView(_ webView: WKWebView, didStartProvisionalNavigation wkNavigation: WKNavigation?) {
         let navigation: Navigation
-        if let expectedNavigation = navigationExpectedToStart, wkNavigation != nil || expectedNavigation.navigationAction.navigationType == .sessionRestoration {
+        if let expectedNavigation = navigationExpectedToStart,
+           wkNavigation != nil
+            || expectedNavigation.navigationAction.navigationType == .sessionRestoration
+            || expectedNavigation.navigationAction.url.scheme.map(URL.NavigationalScheme.init) == .about {
+
             // regular flow: start .expected navigation
             navigation = expectedNavigation
-        } else {
+        } else if webView.url?.isEmpty == false {
             assertionFailure("session restoration happening without NavigationAction")
             navigation = Navigation(identity: NavigationIdentity(wkNavigation), responders: responders, state: .expected(nil), isCurrent: true)
             navigation.navigationActionReceived(.sessionRestoreNavigation(webView: webView, mainFrameNavigation: navigation))
+        } else {
+            return
         }
 
         navigation.started(wkNavigation)
@@ -696,7 +702,7 @@ extension DistributedNavigationDelegate: WKNavigationDelegate {
     @MainActor
     public func webView(_ webView: WKWebView, didCommit wkNavigation: WKNavigation?) {
         guard let navigation = wkNavigation?.navigation ?? startedNavigation else {
-            assertionFailure("Unexpected didCommitNavigation")
+            assert(wkNavigation == nil, "Unexpected didCommitNavigation without preceding didStart")
             return
         }
         updateCurrentHistoryItemIdentity(webView.backForwardList.currentItem)
