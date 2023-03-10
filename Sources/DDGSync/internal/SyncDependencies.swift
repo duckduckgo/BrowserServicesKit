6d0b89aac603db08e4e51acb0a185df8c256b4eb
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-03-10 12:30:37 +0100

    DDGSync module (#253)
    Task/Issue URL: https://app.asana.com/0/72649045549333/1203756807343786/f
    
    Description:
    Add DDGSync module with initial support for Sync API:
    * Signup, login and logout-device endpoints are implemented and proven to work.
    * Endpoints for sending/receiving updates are not supported in the UI yet and are
      subject to change. They are basically in the same shape as implemented by Brindy several months ago.
    * Network communication has been rewritten to use APIRequest.

---
+++ Sources/DDGSync/internal/SyncDependencies.swift
@@ -0,0 +1,103 @@
+//
+//  SyncDependencies.swift
+//
+//  Copyright © 2022 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+protocol SyncDependencies {
+
+    var endpoints: Endpoints { get }
+    var account: AccountManaging { get }
+    var api: RemoteAPIRequestCreating { get }
+    var secureStore: SecureStoring { get }
+    var responseHandler: ResponseHandling { get }
+    var crypter: Crypting { get }
+
+    func createUpdatesSender(_ persistence: LocalDataPersisting) throws -> UpdatesSending
+    func createUpdatesFetcher(_ persistence: LocalDataPersisting) throws -> UpdatesFetching
+
+}
+
+protocol AccountManaging {
+
+    func createAccount(deviceName: String, deviceType: String) async throws -> SyncAccount
+
+    func login(recoveryKey: Data, deviceName: String, deviceType: String) async throws -> (account: SyncAccount, devices: [RegisteredDevice])
+    func logout(deviceId: String, token: String) async throws
+
+}
+
+protocol SecureStoring {
+    func persistAccount(_ account: SyncAccount) throws
+    func account() throws -> SyncAccount?
+    func removeAccount() throws
+}
+
+protocol ResponseHandling {
+    func handleUpdates(_ data: Data) async throws
+}
+
+protocol UpdatesFetching {
+    func fetch() async throws
+}
+
+public protocol Crypting {
+
+    func encryptAndBase64Encode(_ value: String) throws -> String
+    func encryptAndBase64Encode(_ value: String, using secretKey: Data?) throws -> String
+
+    func base64DecodeAndDecrypt(_ value: String) throws -> String
+    func base64DecodeAndDecrypt(_ value: String, using secretKey: Data?) throws -> String
+
+    func createAccountCreationKeys(userId: String, password: String) throws ->
+        AccountCreationKeys
+
+    func extractLoginInfo(recoveryKey: Data) throws -> ExtractedLoginInfo
+
+    func extractSecretKey(protectedSecretKey: Data, stretchedPrimaryKey: Data) throws -> Data
+
+}
+
+extension Crypting {
+    func encryptAndBase64Encode(_ value: String) throws -> String {
+        try encryptAndBase64Encode(value, using: nil)
+    }
+
+    func base64DecodeAndDecrypt(_ value: String) throws -> String {
+        try base64DecodeAndDecrypt(value, using: nil)
+    }
+}
+
+enum HTTPRequestMethod: String {
+    case GET
+    case POST
+    case PATCH
+    case DELETE
+}
+
+struct HTTPResult {
+    let data: Data?
+    let response: HTTPURLResponse
+}
+
+protocol HTTPRequesting {
+    func execute() async throws -> HTTPResult
+}
+
+protocol RemoteAPIRequestCreating {
+    func createRequest(url: URL, method: HTTPRequestMethod, headers: [String: String], parameters: [String: String], body: Data?, contentType: String?) -> HTTPRequesting
+}
