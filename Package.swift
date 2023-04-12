285ce745dce9b0b91694009c4d7d68ed341307e1
Sam Symons <sam@samsymons.com>
2023-04-11 18:29:56 -0700

    Point to a pre-release TRK commit.

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "6.4.3"),
         .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.1.1"),
-        .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "1.2.1"),
+        .package(url: "https://github.com/duckduckgo/TrackerRadarKit", revision: "ee8adbd80611c8ce5bff827d66bf031a32f6fdf5"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "2.1.0"),
         .package(url: "https://github.com/duckduckgo/content-scope-scripts", exact: "4.4.4"),
