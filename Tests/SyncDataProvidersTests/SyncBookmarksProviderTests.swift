48de2f7b28ef8d9c1062c3dc4b1d8be254a8f95b
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-04-28 16:15:49 +0200

    Add more tests for deleting bookmarks

--- Tests/SyncDataProvidersTests/SyncBookmarksProviderTests.swift
+++ Tests/SyncDataProvidersTests/SyncBookmarksProviderTests.swift
@@ -368,6 +368,60 @@ final class SyncBookmarksProviderTests: XCTestCase {
         }
     }
 
+    func testDeletingBookmarks() {
+        let context = bookmarksDatabase.makeContext(concurrencyType: .privateQueueConcurrencyType)
+
+        let bookmarkTree = BookmarksTree {
+            Bookmark(id: "1")
+            Bookmark(id: "2")
+        }
+
+        let received: [Syncable] = [
+            .rootFolder(children: ["2"]),
+            .bookmark(id: "1", isDeleted: true)
+        ]
+
+        context.performAndWait {
+            BookmarkUtils.prepareFoldersStructure(in: context)
+            let rootFolder = bookmarkTree.createEntities(in: context)
+            try? context.save()
+
+            provider.processReceivedBookmarks(received, in: context, using: crypter)
+            try? context.save()
+
+            XCTAssertEqual(rootFolder.childrenArray.map(\.uuid), ["2"])
+        }
+    }
+
+    func testThatDeletesForNonExistendBookmarksAreIgnored() {
+        let context = bookmarksDatabase.makeContext(concurrencyType: .privateQueueConcurrencyType)
+
+        let bookmarkTree = BookmarksTree {
+            Bookmark(id: "1")
+            Bookmark(id: "2")
+        }
+
+        let received: [Syncable] = [
+            .rootFolder(children: ["1", "2"]),
+            .bookmark(id: "3", isDeleted: true),
+            .bookmark(id: "4", isDeleted: true),
+            .bookmark(id: "5", isDeleted: true),
+            .bookmark(id: "6", isDeleted: true),
+            .bookmark(id: "7", isDeleted: true)
+        ]
+
+        context.performAndWait {
+            BookmarkUtils.prepareFoldersStructure(in: context)
+            let rootFolder = bookmarkTree.createEntities(in: context)
+            try? context.save()
+
+            provider.processReceivedBookmarks(received, in: context, using: crypter)
+            try? context.save()
+
+            XCTAssertEqual(rootFolder.childrenArray.map(\.uuid), ["1", "2"])
+        }
+    }
+
     func testDeletingAndReordering() {
         let context = bookmarksDatabase.makeContext(concurrencyType: .privateQueueConcurrencyType)
 
