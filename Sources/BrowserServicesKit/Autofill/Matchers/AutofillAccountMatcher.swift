8b5047449ffa800c73e70f8fb62c25a2c8b4674e
Daniel Bernal <1156669+afterxleep@users.noreply.github.com>
2023-05-09 15:12:36 +0200

    Revert AccountMatcher removal

--- Sources/BrowserServicesKit/Autofill/Matchers/AutofillAccountMatcher.swift
+++ Sources/BrowserServicesKit/Autofill/Matchers/AutofillAccountMatcher.swift
@@ -20,7 +20,19 @@
 import Foundation
 import Common
 
+public struct AccountMatches {
+    public let perfectMatches: [SecureVaultModels.WebsiteAccount]
+    public let partialMatches: [String: [SecureVaultModels.WebsiteAccount]]
+
+    public init(perfectMatches: [SecureVaultModels.WebsiteAccount],
+                partialMatches: [String: [SecureVaultModels.WebsiteAccount]]) {
+        self.perfectMatches = perfectMatches
+        self.partialMatches = partialMatches
+    }
+}
+
 public protocol AutofillAccountMatcher {
+    func findMatchesSortedByLastUpdated(accounts: [SecureVaultModels.WebsiteAccount], for url: String) -> AccountMatches
     func findMatches(accounts: [SecureVaultModels.WebsiteAccount], for url: String) -> [SecureVaultModels.WebsiteAccount]
 }
 
@@ -34,8 +46,46 @@ public struct AutofillWebsiteAccountMatcher: AutofillAccountMatcher {
         self.tld = tld
     }
 
+    @available(*, deprecated, message: "Use findMatches(accounts:for:) -- Which returns an already sorted list with removed duplicates")
+    public func findMatchesSortedByLastUpdated(accounts: [SecureVaultModels.WebsiteAccount], for url: String) -> AccountMatches {
+        let matches = buildMatches(accounts: accounts, for: url)
+        return sort(matches: matches)
+    }
+
+    /// Builds a list of accounts that are perfect matches for the given url
+    /// and a dictionary of groups of accounts with the same subdomain that are partial matches for the given url.
+    private func buildMatches(accounts: [SecureVaultModels.WebsiteAccount], for url: String) -> AccountMatches {
+        var perfectMatches = [SecureVaultModels.WebsiteAccount]()
+        var partialMatches = [String: [SecureVaultModels.WebsiteAccount]]()
+        let currentUrlComponents = autofillUrlMatcher.normalizeSchemeForAutofill(url)
+
+        for account in accounts {
+            if let savedUrlComponents = autofillUrlMatcher.normalizeSchemeForAutofill(account.domain) {
+                if !autofillUrlMatcher.isMatchingForAutofill(currentSite: url, savedSite: account.domain, tld: tld) {
+                    continue
+                }
+
+                if currentUrlComponents?.subdomain(tld: tld) == savedUrlComponents.subdomain(tld: tld) {
+                    perfectMatches.append(account)
+                } else {
+                    partialMatches[account.domain, default: []].append(account)
+                }
+            }
+        }
+
+        return AccountMatches(perfectMatches: perfectMatches, partialMatches: partialMatches)
+    }
+
+    private func sort(matches: AccountMatches) -> AccountMatches {
+        let sortedPerfectMatches = matches.perfectMatches.sorted { $0.lastUpdated > $1.lastUpdated }
+        let partialMatches = matches.partialMatches.mapValues { $0.sorted { $0.lastUpdated > $1.lastUpdated } }
+        return AccountMatches(perfectMatches: sortedPerfectMatches, partialMatches: partialMatches)
+    }
+
+
     public func findMatches(accounts: [SecureVaultModels.WebsiteAccount], for url: String) -> [SecureVaultModels.WebsiteAccount] {
         return accounts.sortedForDomain(url, tld: tld, removeDuplicates: true)
     }
-    
+
 }
+
