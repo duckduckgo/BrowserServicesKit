54458ba52685fe3145dbeebd2843445bc802d129
Shane Osbourne <sosbourne@duckduckgo.com>
2023-05-23 10:50:03 +0100

    feature: support message brokering for user scripts

---
+++ Sources/UserScript/UserScriptMessagingSchema.swift
@@ -0,0 +1,200 @@
+//
+//  UserScriptMessagingSchema.swift
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+import Foundation
+
+/// Fire-and-forget notifications from the user script
+///
+/// https://duckduckgo.github.io/content-scope-scripts/classes/Messaging_Schema.NotificationMessage.html
+public struct NotificationMessage {
+    public let context: String
+    public let featureName: String
+    public let method: String
+    public let params: Any
+}
+
+/// A RequestMessage is just a NotificationMessage but with an 'id' field
+/// The presence of the 'id' field indicates that a response is expected
+///
+/// https://duckduckgo.github.io/content-scope-scripts/classes/Messaging_Schema.RequestMessage.html
+public struct RequestMessage {
+    public let context: String
+    public let featureName: String
+    public let id: String
+    public let method: String
+    public let params: Any
+}
+
+/// Sent in response to a `RequestMessage`
+///
+/// https://duckduckgo.github.io/content-scope-scripts/classes/Messaging_Schema.MessageResponse.html
+public struct MessageResponse {
+    public let context: String
+    public let featureName: String
+    public let id: String
+    public let result: Encodable
+
+    public static func toJSON(request: RequestMessage, result: Encodable) -> String? {
+
+        // construct a 'MessageResponse' -> this is done to verify the types against the schema
+        let res = MessageResponse(
+            context: request.context,
+            featureName: request.featureName,
+            id: request.id,
+            result: result
+        )
+
+        // I added this because I couldn't figure out the generic/recursive Encodable
+        let dictionary: [String: Encodable] = [
+            "context": res.context,
+            "featureName": res.featureName,
+            "id": res.id,
+            "result": res.result
+        ]
+
+        return GenericJSONOutput.toJSON(dict: dictionary)
+    }
+}
+
+/// Like a MessageResponse, except it has a 'MessageError' instead of a result
+///
+/// https://duckduckgo.github.io/content-scope-scripts/classes/Messaging_Schema.MessageResponse.html
+public struct MessageErrorResponse: Encodable {
+    public let context: String
+    public let featureName: String
+    public let id: String
+    public let error: MessageError
+
+    public static func forRequest(request: RequestMessage, error: ResponseError) -> Self {
+        MessageErrorResponse(context: request.context, featureName: request.featureName, id: request.id, error: MessageError(message: error.localizedDescription))
+    }
+
+    public func toJSON() -> String {
+        let jsonData = try! JSONEncoder().encode(self)
+        let jsonString = String(data: jsonData, encoding: .utf8)!
+        return jsonString
+    }
+}
+
+public enum ResponseError: Error {
+    case missingEncodableResult
+    case jsonEncodingFailed
+    case otherError(Error)
+}
+
+extension ResponseError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .missingEncodableResult:
+            return "could not access encodable result"
+        case .jsonEncodingFailed:
+            return "could not convert result to json"
+        case .otherError(let error):
+            return error.localizedDescription
+        }
+    }
+}
+
+/// https://duckduckgo.github.io/content-scope-scripts/classes/Messaging_Schema.MessageError.html
+public struct MessageError: Encodable {
+    public let message: String
+}
+
+/// Use this format to push data into UserScript
+///
+/// https://duckduckgo.github.io/content-scope-scripts/classes/Messaging_Schema.SubscriptionEvent.html
+public struct SubscriptionEvent {
+    public let context: String
+    public let featureName: String
+    public let subscriptionName: String
+    public let params: Encodable
+
+    public func toJSON() -> String? {
+
+        // I added this because I couldn't figure out the generic/recursive Encodable
+        let dictionary: [String: Encodable] = [
+            "context": self.context,
+            "featureName": self.featureName,
+            "subscriptionName": self.subscriptionName,
+            "params": self.params
+        ]
+
+        return GenericJSONOutput.toJSON(dict: dictionary)
+    }
+
+    public static func toJS(context: String, featureName: String, subscriptionName: String, params: Encodable) -> String? {
+
+        let res = SubscriptionEvent(context: context, featureName: featureName, subscriptionName: subscriptionName, params: params)
+        guard let json = res.toJSON() else {
+            assertionFailure("Could not convert a SubscriptionEvent to JSON")
+            return nil
+        }
+
+        return """
+           (() => {
+              if (!('\(res.subscriptionName)' in window)) {
+                 console.warn("missing '\(res.subscriptionName)'", \(json))
+              } else {
+                  window.\(res.subscriptionName)?.(\(json));
+              }
+           })();
+           """
+    }
+}
+
+/// https://duckduckgo.github.io/content-scope-scripts/classes/Messaging.WebkitMessagingConfig.html
+public struct WebkitMessagingConfig: Encodable {
+    let webkitMessageHandlerNames: [String]
+    let secret: String
+    var hasModernWebkitAPI: Bool
+}
+
+// helper types for nested JSON output (please remove if there's a better way
+struct GenericJSONOutput: Encodable {
+    let dict: [String: Encodable]
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: AnyCodingKey.self)
+        for (key, value) in dict {
+            let codingKey = AnyCodingKey(stringValue: key)!
+            try container.encode(value, forKey: codingKey)
+        }
+    }
+
+    static func toJSON(dict: [String: Encodable]) -> String? {
+        let myData = GenericJSONOutput(dict: dict)
+        let encoder = JSONEncoder()
+
+        guard let jsonData = try? encoder.encode(myData),
+              let jsonString = String(data: jsonData, encoding: .utf8) else {
+            return nil
+        }
+
+        return jsonString
+    }
+}
+
+struct AnyCodingKey: CodingKey {
+    var stringValue: String
+    var intValue: Int? { nil }
+
+    init?(stringValue: String) {
+        self.stringValue = stringValue
+    }
+
+    init?(intValue: Int) { nil }
+}
---
+++ Tests/UserScriptTests/UserScriptMessagingTests.swift
@@ -0,0 +1,286 @@
+//
+//  ContentScopeMessagingTests.swift
+//  DuckDuckGo
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import XCTest
+import WebKit
+@testable import UserScript
+
+class UserScriptMessagingTests: XCTestCase {
+
+    /// When an 'id' field is present on an incoming message, it means
+    /// that the client is expecting a response. This test ensures that
+    /// a 'result' keys exists on the response, as per [MessageResponse](https://duckduckgo.github.io/content-scope-scripts/classes/Messaging_Schema.MessageResponse.html)
+    func testDelegateRespondsWithResult() async {
+        let (testee, action, original) = setupWith(message: [
+            "context": "any_context",
+            "featureName": "fooBarFeature",
+            "method": "responseExample",
+            "id": "abcdef01623456",
+            "params": [
+                "name": "kittie"
+            ]
+        ])
+
+        let expectation = XCTestExpectation(description: "replyHandler called")
+
+        do {
+            let json = try await testee.execute(action: action, original: original)
+
+            // convert back from json to test the e2e flow
+            if let data = json.data(using: .utf8),
+                let obj = try? JSONSerialization.jsonObject(with: data),
+                let dict = obj as? [String: Any],
+                let result = dict["result"] as? [String: Any] {
+
+                XCTAssertEqual(dict["context"] as? String, testee.context)
+                XCTAssertEqual(dict["featureName"] as? String, "fooBarFeature")
+                XCTAssertEqual(dict["id"] as? String, "abcdef01623456")
+
+                // here we care that the data was returned inside 'result'
+                XCTAssertEqual(result["name"] as? String, "Kittie")
+
+                expectation.fulfill()
+            }
+        } catch {}
+
+        wait(for: [expectation], timeout: 2.0)
+    }
+
+    /// This test verifies that the replyHandler is called for notifications,
+    /// but that it just contains an empty Object. This is important because it
+    /// prevents the Promise from being rejected on the JS side.
+    ///
+    /// Note: the incoming message has no `id` field. This is what makes it a 'notification'
+    func testDelegateHandlesNotification() async {
+
+        let (testee, action, original) = setupWith(message: [
+            "context": "any_context",
+            "featureName": "fooBarFeature",
+            "method": "notifyExample",
+            "params": [
+                "name": "kittie"
+            ]
+        ])
+
+        // expectation waiter
+        let expectation = XCTestExpectation(description: "replyHandler called")
+
+        do {
+            let json = try await testee.execute(action: action, original: original)
+
+            // in a notification, we send back an empty object to prevent the promise from rejecting on the client
+            XCTAssertEqual("{}", json)
+            expectation.fulfill()
+        } catch {}
+
+        wait(for: [expectation], timeout: 2.0)
+    }
+
+    /// This test verifies that errors from handlers are reflected back to the JS side.
+    /// This is an important part of the flow - because when a `request` is sent from the JS
+    /// it can use the error-response to close the loop between request/response.
+    func testDelegateRespondsWithErrorResponse() async {
+
+        let (testee, action, original) = setupWith(message: [
+            "context": "any_context",
+            "featureName": "fooBarFeature",
+            "method": "errorExample",
+            "id": "abcdef01623456",
+            "params": [
+                "name": "kittie"
+            ]
+        ])
+
+        let expectation = XCTestExpectation(description: "replyHandler called")
+
+        do {
+            let json = try await testee.execute(action: action, original: original)
+            if let data = json.data(using: .utf8),
+               let obj = try? JSONSerialization.jsonObject(with: data),
+               let dict = obj as? [String: Any],
+               let error = dict["error"] as? [String: Any] {
+
+                XCTAssertEqual(dict["context"] as? String, testee.context)
+                XCTAssertEqual(dict["featureName"] as? String, "fooBarFeature")
+                XCTAssertEqual(dict["id"] as? String, "abcdef01623456")
+
+                // we care that 'error.message' is reflected to the client
+                XCTAssertEqual(error["message"] as? String, "Some Error")
+
+                expectation.fulfill()
+            }
+        } catch {}
+
+        wait(for: [expectation], timeout: 2.0)
+    }
+
+    /// Ensure that an error is thrown if the feature was not registered
+    func testThrowsOnMissingFeature() async {
+        let (testee, action, original) = setupWith(message: [
+            "context": "any_context",
+            "featureName": "this_feature_doesnt_exist",
+            "method": "an_unknown_method_name_but_no_id",
+            "params": [
+                "foo": "bar"
+            ]
+        ])
+
+        do {
+            _ = try await testee.execute(action: action, original: original)
+        } catch let error {
+            XCTAssertEqual(error.localizedDescription, "feature named `this_feature_doesnt_exist` was not found")
+        }
+    }
+
+    /// Ensure that an error is thrown if a feature was found, but it wasn't able to select a handler for the
+    /// particular incoming 'method'
+    func testThrowsOnMissingMethod() async {
+        let (testee, action, original) = setupWith(message: [
+            "context": "any_context",
+            "featureName": "fooBarFeature",
+            "method": "an_unknown_method_name_but_no_id",
+            "params": [
+                "foo": "bar"
+            ]
+        ])
+
+        do {
+            _ = try await testee.execute(action: action, original: original)
+        } catch let error {
+            XCTAssertEqual(error.localizedDescription, "the incoming message is ignored because the feature `fooBarFeature` couldn't provide a handler for method `an_unknown_method_name_but_no_id`")
+        }
+    }
+
+    /// Ensure that an error is thrown if the `context` key is absent. `context` is not *technically* needed
+    /// on the webkit implementation (because the webkit MessageHandler must of been correct), this test is
+    /// more about compliance with the shared/documented types
+    func testThrowsOnMissingContext() async {
+        let (testee, action, original) = setupWith(message: [
+            "featureName": "fooBarFeature",
+            "method": "an_unknown_method_name_but_no_id",
+            "params": [
+                "foo": "bar"
+            ]
+        ])
+
+        do {
+            _ = try await testee.execute(action: action, original: original)
+        } catch {
+            XCTAssertEqual(error.localizedDescription, "The incoming message was not valid - one or more of 'featureName', 'method'  or 'context' was missing")
+        }
+    }
+}
+
+/// A helper for registering a test delegate and creating a MockMsg based on the
+/// incoming dictionary (which represents a message coming from a webview)
+///
+/// - Parameter message: The incoming message
+///
+func setupWith(message: [String: Any]) -> (UserScriptMessageBroker, UserScriptMessageBroker.Action, WKScriptMessage) {
+    // create the instance of ContentScopeMessaging
+    let testee = UserScriptMessageBroker(context: message["context"] as? String ?? "default")
+
+    // register a feature for a given name
+    testee.registerSubFeature(delegate: TestDelegate())
+
+    // Mock the call from the webview.
+    let msg1 = MockMsg(name: testee.context, body: message)
+
+    // get the handler action
+    let action = testee.messageHandlerFor(msg1)
+
+    return (testee, action, msg1)
+}
+
+/// An example of how to conform to `ContentScopeScriptsSubFeature`
+/// It contains 3 examples that are typical of a feature that needs to
+/// communicate to a UserScript
+struct TestDelegate: Subfeature {
+    weak var broker: UserScriptMessageBroker?
+
+    var featureName = "fooBarFeature"
+
+    /// This feature will accept messages from .all - meaning every origin
+    ///
+    /// Some features may want to restrict the origin's that it accepts messages from
+    var messageOriginPolicy: MessageOriginPolicy = .all
+
+    /// An example of how to provide different handlers bad on
+    func handler(forMethodNamed methodName: String) -> Subfeature.Handler? {
+        switch methodName {
+        case "notifyExample": return notifyExample
+        case "errorExample": return errorExample
+        case "responseExample": return responseExample
+        default:
+            return nil
+        }
+    }
+
+    /// An example of a simple Encodable data type that can be used directly in replies
+    struct Person: Encodable {
+        let name: String
+    }
+
+    /// An example that represents handling a [NotificationMessage](https://duckduckgo.github.io/content-scope-scripts/classes/Messaging_Schema.NotificationMessage.html)
+    func notifyExample(params: Any, original: WKScriptMessage) async throws -> Encodable? {
+        print("not replying...")
+        return nil
+    }
+
+    /// An example that represents throwing an exception from a handler
+    ///
+    /// Note: if this happens as part of a 'request', the error string will be forwarded onto the client side JS
+    func errorExample(params: Any, original: WKScriptMessage) async throws -> Encodable? {
+        let error = NSError(domain: "MyHandler", code: 0, userInfo: [NSLocalizedDescriptionKey: "Some Error"])
+        throw error
+    }
+
+    /// An example of how a handler can reply with any Encodable data type
+    func responseExample(params: Any, original: WKScriptMessage) async throws -> Encodable? {
+        let person = Person(name: "Kittie")
+        return person
+    }
+}
+
+class MockMsg: WKScriptMessage {
+
+    let mockedName: String
+    let mockedBody: Any
+    let mockedWebView: WKWebView?
+
+    override var name: String {
+        return mockedName
+    }
+
+    override var body: Any {
+        return mockedBody
+    }
+
+    override var webView: WKWebView? {
+        return mockedWebView
+    }
+
+    init(name: String, body: Any, webView: WKWebView? = nil) {
+        self.mockedName = name
+        self.mockedBody = body
+        self.mockedWebView = webView
+        super.init()
+    }
+}
