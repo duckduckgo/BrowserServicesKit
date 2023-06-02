871cb3f23044adeb4c3acaa949856ea350726e21
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-06-02 10:35:18 +0200

    Add EventMapping to DDGSync dependencies and call it from handleUnauthenticated

--- Sources/DDGSync/internal/SyncDependencies.swift
+++ Sources/DDGSync/internal/SyncDependencies.swift
@@ -28,6 +28,7 @@ protocol SyncDependencies {
     var secureStore: SecureStoring { get }
     var crypter: CryptingInternal { get }
     var scheduler: SchedulingInternal { get }
+    var errorEvents: EventMapping<SyncError> { get }
     var log: OSLog { get }
 
     func createRemoteConnector(_ connectInfo: ConnectInfo) throws -> RemoteConnecting
