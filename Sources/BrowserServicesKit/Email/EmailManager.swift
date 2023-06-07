396a849eefeb2cc42f1cb353499392bdd59f106f
Daniel Bernal <1156669+afterxleep@users.noreply.github.com>
2023-06-07 19:08:37 +0200

    Error handling in Request Delegate

--- Sources/BrowserServicesKit/Email/EmailManager.swift
+++ Sources/BrowserServicesKit/Email/EmailManager.swift
@@ -83,6 +83,11 @@ public protocol EmailManagerAliasPermissionDelegate: AnyObject {
 }
 // swiftlint:enable identifier_name
 
+public enum EmailManagerRequestDelegateError: Error {
+    case serverError(statusCode: Int)
+    case decodingError
+}
+
 // swiftlint:disable function_parameter_count
 public protocol EmailManagerRequestDelegate: AnyObject {
 
@@ -133,6 +138,14 @@ public typealias AliasCompletion = (String?, AliasRequestError?) -> Void
 public typealias UsernameAndAliasCompletion = (_ username: String?, _ alias: String?, AliasRequestError?) -> Void
 public typealias UserDataCompletion = (_ username: String?, _ alias: String?, _ token: String?, AliasRequestError?) -> Void
 
+public enum EmailAliasStatus {
+    case active
+    case inactive
+    case notFound
+    case error
+    case unknown
+}
+
 public class EmailManager {
     
     private static let emailDomain = "duck.com"
@@ -144,7 +157,7 @@ public class EmailManager {
     public enum NotificationParameter {
         public static let cohort = "cohort"
     }
-    
+
     private lazy var emailUrls = EmailUrls()
     private lazy var aliasAPIURL = emailUrls.emailAliasAPI
 
@@ -297,7 +310,7 @@ public class EmailManager {
         }
     }
 
-    public func getStatusFor(email: String, timeoutInterval: TimeInterval = 4.0) async throws -> Bool {
+    public func getStatusFor(email: String, timeoutInterval: TimeInterval = 4.0) async throws -> EmailAliasStatus {
         do {
             return try await fetchStatusFor(alias: aliasFor(email), timeoutInterval: timeoutInterval)
         }
@@ -555,7 +568,7 @@ private extension EmailManager {
         }
     }
 
-    func fetchStatusFor(alias: String, timeoutInterval: TimeInterval = 5.0) async throws -> Bool {
+    func fetchStatusFor(alias: String, timeoutInterval: TimeInterval = 5.0) async throws -> EmailAliasStatus {
         guard isSignedIn,
               let requestDelegate else {
             throw AliasRequestError.signedOut
@@ -574,9 +587,19 @@ private extension EmailManager {
                                                           httpBody: body,
                                                           timeoutInterval: timeoutInterval)
             let response: EmailAliasStatusResponse = try JSONDecoder().decode(EmailAliasStatusResponse.self, from: data)
-            return response.active
-        } catch {
-            throw AliasRequestError.invalidResponse
+            return response.active ? .active : .inactive
+        } catch let error {
+            switch error {
+                case EmailManagerRequestDelegateError.serverError(let code):
+                    switch code {
+                        case 404:
+                            return .notFound
+                        default:
+                            return .error
+                    }
+                default:
+                    return .error
+            }
         }
     }
 
