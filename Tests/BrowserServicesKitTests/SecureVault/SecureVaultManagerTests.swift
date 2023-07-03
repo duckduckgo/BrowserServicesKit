8e9266ec41bec9284002e67ab4378e1bb1e932d6
Daniel Bernal <1156669+afterxleep@users.noreply.github.com>
2023-07-03 19:37:08 +0200

    Updated first batch of tests

--- Tests/BrowserServicesKitTests/SecureVault/SecureVaultManagerTests.swift
+++ Tests/BrowserServicesKitTests/SecureVault/SecureVaultManagerTests.swift
@@ -66,7 +66,7 @@ class SecureVaultManagerTests: XCTestCase {
     
     func testWhenGettingExistingEntries_AndNoAutofillDataWasProvided_AndNoEntriesExist_ThenReturnValueIsNil() throws {
         let autofillData = AutofillUserScript.DetectedAutofillData(identity: nil, credentials: nil, creditCard: nil, trigger: nil)
-        let entries = try manager.existingEntries(for: "domain.com", autofillData: autofillData, automaticallySavedCredentials: false, shouldSilentlySave: false)
+        let entries = try manager.existingEntries(for: "domain.com", autofillData: autofillData)
         
         XCTAssertNil(entries.credentials)
         XCTAssertNil(entries.identity)
@@ -77,7 +77,7 @@ class SecureVaultManagerTests: XCTestCase {
         let card = paymentMethod(cardNumber: "5555555555555557", cardholderName: "Name", cvv: "123", month: 1, year: 2022)
 
         let autofillData = AutofillUserScript.DetectedAutofillData(identity: nil, credentials: nil, creditCard: card, trigger: nil)
-        let entries = try manager.existingEntries(for: "domain.com", autofillData: autofillData, automaticallySavedCredentials: false, shouldSilentlySave: false)
+        let entries = try manager.existingEntries(for: "domain.com", autofillData: autofillData)
         
         XCTAssertNil(entries.credentials)
         XCTAssertNil(entries.identity)
@@ -90,7 +90,7 @@ class SecureVaultManagerTests: XCTestCase {
         try self.testVault.storeCreditCard(card)
 
         let autofillData = AutofillUserScript.DetectedAutofillData(identity: nil, credentials: nil, creditCard: card, trigger: nil)
-        let entries = try manager.existingEntries(for: "domain.com", autofillData: autofillData, automaticallySavedCredentials: false, shouldSilentlySave: false)
+        let entries = try manager.existingEntries(for: "domain.com", autofillData: autofillData)
         
         XCTAssertNil(entries.credentials)
         XCTAssertNil(entries.identity)
@@ -101,7 +101,7 @@ class SecureVaultManagerTests: XCTestCase {
         let identity = identity(name: ("First", "Middle", "Last"), addressStreet: "Address Street")
         
         let autofillData = AutofillUserScript.DetectedAutofillData(identity: identity, credentials: nil, creditCard: nil, trigger: nil)
-        let entries = try manager.existingEntries(for: "domain.com", autofillData: autofillData, automaticallySavedCredentials: false, shouldSilentlySave: false)
+        let entries = try manager.existingEntries(for: "domain.com", autofillData: autofillData)
         
         XCTAssertNil(entries.credentials)
         XCTAssertNil(entries.creditCard)
@@ -114,7 +114,7 @@ class SecureVaultManagerTests: XCTestCase {
         try self.testVault.storeIdentity(identity)
 
         let autofillData = AutofillUserScript.DetectedAutofillData(identity: identity, credentials: nil, creditCard: nil, trigger: nil)
-        let entries = try manager.existingEntries(for: "domain.com", autofillData: autofillData, automaticallySavedCredentials: false, shouldSilentlySave: false)
+        let entries = try manager.existingEntries(for: "domain.com", autofillData: autofillData)
         
         XCTAssertNil(entries.credentials)
         XCTAssertNil(entries.identity)
@@ -341,6 +341,10 @@ private class MockSecureVaultManagerDelegate: SecureVaultManagerDelegate {
     func secureVaultManagerIsEnabledStatus(_: SecureVaultManager) -> Bool {
         return true
     }
+
+    func secureVaultManager(_: SecureVaultManager,
+                            promptUserToStoreAutofillData data: AutofillData,
+                            withTrigger trigger: AutofillUserScript.GetTriggerType?) {}
 
     func secureVaultManager(_: SecureVaultManager,
                             promptUserToAutofillCredentialsForDomain domain: String,
