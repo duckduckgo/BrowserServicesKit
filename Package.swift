a1ab5637050156f800914e641d75dec97d3652e8
Jacek Łyp <jlyp@duckduckgo.com>
2023-07-03 22:02:42 +0200

    Use stable version instead of branch for bloom filter ref (#404)
    Task/Issue URL: https://app.asana.com/0/903182762228930/1202443550575711/f

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
         .package(url: "https://github.com/duckduckgo/content-scope-scripts", exact: "4.22.4"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "1.4.0"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
-        .package(url: "https://github.com/duckduckgo/bloom_cpp.git", branch: "main"),
+        .package(url: "https://github.com/duckduckgo/bloom_cpp.git", exact: "3.0.0"),
     ],
     targets: [
         .target(
