89ade55a62fb38efa0f95f4c83474a49b6ab2303
Shane Osbourne <sosbourne@duckduckgo.com>
2023-07-11 19:30:44 +0100

    update to CCF to released version 4.22.4

--- Package.swift
+++ Package.swift
@@ -29,10 +29,10 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "7.2.0"),
         .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.1.1"),
-        .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "1.2.1"),
+        .package(url: "https://github.com/duckduckgo/TrackerRadarKit.git", exact: "1.2.1"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "2.1.0"),
-        .package(url: "https://github.com/duckduckgo/content-scope-scripts", branch: "shane/duckplayer-shorts-enabled"),
+        .package(url: "https://github.com/duckduckgo/content-scope-scripts", exact: "4.24.0"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "1.4.0"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
         .package(url: "https://github.com/duckduckgo/bloom_cpp.git", exact: "3.0.0"),
