84cf076cb8b9b9d733c59a89ee5536296c38a7d2
Graeme Arthur <2030310+graeme@users.noreply.github.com>
2023-07-17 13:28:17 +0200

    NetP: Move bundle prefix to x-platform code (#418)
    Task/Issue URL: https://app.asana.com/0/0/1205043071738197/f
    iOS PR: duckduckgo/iOS#1835
    macOS PR: duckduckgo/macos-browser#1356
    What kind of version bump will this require?: Minor
    
    Description:
    
    Sadly, on trying to open the linked iOS PR (which wasn’t supposed to need BSK changes), I hit a snag as some iOS incompatible changes made there way into BSK and didn’t get caught by CI (it wasn’t actually being used yet on iOS for the sake of keeping PRs manageable, so it didn’t get caught in the app either). So when I went to create my final PR, it wasn't compiling.

--- Sources/NetworkProtection/Notifications/NetworkProtectionNotification.swift
+++ Sources/NetworkProtection/Notifications/NetworkProtectionNotification.swift
@@ -21,18 +21,6 @@ import Common
 
 #if os(macOS)
 
-fileprivate extension Bundle {
-    private static let networkProtectionDistributedNotificationPrefixKey = "DISTRIBUTED_NOTIFICATIONS_PREFIX"
-
-    var networkProtectionDistributedNotificationPrefix: String {
-        guard let bundleID = object(forInfoDictionaryKey: Self.networkProtectionDistributedNotificationPrefixKey) as? String else {
-            fatalError("Info.plist is missing \(Self.networkProtectionDistributedNotificationPrefixKey)")
-        }
-
-        return bundleID
-    }
-}
-
 extension DistributedNotificationCenter {
     // MARK: - Logging
 
@@ -55,6 +43,18 @@ extension DistributedNotificationCenter: NetworkProtectionNotificationPosting {
 }
 
 #endif
+
+fileprivate extension Bundle {
+    private static let networkProtectionDistributedNotificationPrefixKey = "DISTRIBUTED_NOTIFICATIONS_PREFIX"
+
+    var networkProtectionDistributedNotificationPrefix: String {
+        guard let bundleID = object(forInfoDictionaryKey: Self.networkProtectionDistributedNotificationPrefixKey) as? String else {
+            fatalError("Info.plist is missing \(Self.networkProtectionDistributedNotificationPrefixKey)")
+        }
+
+        return bundleID
+    }
+}
 
 public protocol NetworkProtectionNotificationPosting: AnyObject {
     func post(_ networkProtectionNotification: NetworkProtectionNotification, object: String?, log: OSLog)
