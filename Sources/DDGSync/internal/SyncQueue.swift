141cd7a7fc0719408ab0b84c9a90549a4c838623
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-07-24 23:57:39 +0200

    Fix needsRemoteDataFetch

--- Sources/DDGSync/internal/SyncQueue.swift
+++ Sources/DDGSync/internal/SyncQueue.swift
@@ -96,9 +96,13 @@ class SyncQueue {
     }
 
     func prepareNewDataModelsForFirstSync(needRemoteDataFetch: Bool) throws {
-        for dataProvider in dataProviders where !dataProvider.isFeatureRegistered {
+        let unregisteredDataProviders = dataProviders.filter { !$0.isFeatureRegistered }
+        let didAddNewModelsToSync = unregisteredDataProviders.count != dataProviders.count
+        let needsRemoteDataFetch = needRemoteDataFetch || didAddNewModelsToSync
+
+        for dataProvider in unregisteredDataProviders {
             do {
-                try dataProvider.prepareForFirstSync(needsRemoteDataFetch: needRemoteDataFetch)
+                try dataProvider.prepareForFirstSync(needsRemoteDataFetch: needsRemoteDataFetch)
             } catch {
                 os_log(.debug, log: self.log, "Error when preparing %{public}s for first sync: %{public}s", dataProvider.feature.name, error.localizedDescription)
                 dataProvider.handleSyncError(error)
