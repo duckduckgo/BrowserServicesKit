edfdb5f0c9076e731efb4eee435a5ed186acee33
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-07-28 10:30:45 +0200

    Remove commented out code

--- Tests/SyncDataProvidersTests/Credentials/helpers/TestAutofillSecureVaultFactory.swift
+++ Tests/SyncDataProvidersTests/Credentials/helpers/TestAutofillSecureVaultFactory.swift
@@ -21,15 +21,6 @@ import Foundation
 import SecureStorage
 @testable import BrowserServicesKit
 
-//extension DefaultDatabaseProvider {
-//    static let testKey = "test-key".data(using: .utf8)!
-//
-//    static func makeTestProvider() throws -> DefaultDatabaseProvider {
-//        let databaseLocation = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString + ".db")
-//        return try DefaultDatabaseProvider(file: databaseLocation, key: testKey)
-//    }
-//}
-
 extension AutofillVaultFactory {
     static func testFactory(databaseProvider: DefaultAutofillDatabaseProvider) -> AutofillVaultFactory {
         AutofillVaultFactory(makeCryptoProvider: {
@@ -44,29 +35,3 @@ extension AutofillVaultFactory {
         })
     }
 }
-
-//class TestSecureVaultFactory: AutofillVaultFactory {
-//
-//    var mockCryptoProvider = NoOpCryptoProvider()
-//    var mockKeystoreProvider = MockKeystoreProvider()
-//    var databaseProvider: DefaultDatabaseProvider
-//
-//    init(databaseProvider: DefaultDatabaseProvider) {
-//        self.databaseProvider = databaseProvider
-//        mockKeystoreProvider._l1Key = "l1".data(using: .utf8)
-//        mockKeystoreProvider._encryptedL2Key = "encrypted".data(using: .utf8)
-//        super.init()
-//    }
-//
-//    override func makeCryptoProvider() -> SecureVaultCryptoProvider {
-//        mockCryptoProvider
-//    }
-//
-//    override func makeKeyStoreProvider() -> SecureVaultKeyStoreProvider {
-//        mockKeystoreProvider
-//    }
-//
-//    override func makeDatabaseProvider(key: Data) throws -> SecureVaultDatabaseProvider {
-//        databaseProvider
-//    }
-//}
