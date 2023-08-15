e82d8c1daba1c85a93104e94973754c890684f33
Sam Symons <sam@samsymons.com>
2023-08-15 14:05:46 -0700

    TEMPORARY: Allow fetching from jsonblob.

--- Sources/Configuration/ConfigurationFetching.swift
+++ Sources/Configuration/ConfigurationFetching.swift
@@ -135,7 +135,7 @@ public final class ConfigurationFetcher: ConfigurationFetching {
         let log = log
         let request = APIRequest(configuration: configuration, requirements: requirements, urlSession: urlSession, log: log)
         let (data, response) = try await request.fetch()
-        return (response.etag!, data)
+        return ("fake-etag", data)
     }
 
     private func store(_ result: ConfigurationFetchResult, for configuration: Configuration) throws {
