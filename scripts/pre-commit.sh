1c1d7fe484f380908664b333f43b57e25fcf79d3
Daniel Bernal <1156669+afterxleep@users.noreply.github.com>
2023-08-22 15:30:05 +0200

    Convert indentation to tabs

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -6,42 +6,42 @@ HOOK_PATH="$HOOK_DIR/pre-commit"
 LINTER_SCRIPT_PATH="lint.sh"
 
 install_hook() {
-  # Remove any pre-commit hook that might already be installed
-  rm -f "${HOOK_PATH}"
+	# Remove any pre-commit hook that might already be installed
+	rm -f "${HOOK_PATH}"
 
-  # Define the hook
-  cat > "${HOOK_PATH}" <<- EOF
-  #!/bin/sh
+	# Define the hook
+	cat > "${HOOK_PATH}" <<- EOF
+	#!/bin/sh
 
-  # Run the linter script
-  sh $LINTER_SCRIPT_PATH --fix
+	# Run the linter script
+	sh $LINTER_SCRIPT_PATH --fix
 
-  git add -u
-  echo 'SwiftLint finished fixing files. Proceeding with commit...'
-  EOF
+	git add -u
+	echo 'SwiftLint finished fixing files. Proceeding with commit...'
+	EOF
 
-  # Make the file executable
-  chmod +x "$HOOK_PATH"
+	# Make the file executable
+	chmod +x "$HOOK_PATH"
 
-  echo "Pre-commit hook installed successfully!"
+	echo "Pre-commit hook installed successfully!"
 }
 
 uninstall_hook() {
-  # Remove the pre-commit hook if it exists
-  if [ -f "$HOOK_PATH" ]; then
-    rm "$HOOK_PATH"
-    echo "Pre-commit hook uninstalled successfully!"
-  else
-    echo "No pre-commit hook found to uninstall."
-  fi
+	# Remove the pre-commit hook if it exists
+	if [ -f "$HOOK_PATH" ]; then
+		rm "$HOOK_PATH"
+		echo "Pre-commit hook uninstalled successfully!"
+	else
+		echo "No pre-commit hook found to uninstall."
+	fi
 }
 
 # Check command-line options
 if [ "$1" == "--install" ]; then
-  install_hook
+	install_hook
 elif [ "$1" == "--uninstall" ]; then
-  uninstall_hook
+	uninstall_hook
 else
-  echo "Usage: $0 --install | --uninstall"
-  exit 1
+	echo "Usage: $0 --install | --uninstall"
+	exit 1
 fi
