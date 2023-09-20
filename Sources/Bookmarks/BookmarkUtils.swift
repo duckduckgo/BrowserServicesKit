3769cd912b9de35bb1347c6367dc1e7c61257cf5
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-09-20 14:43:42 +0200

    Add a helper function that cleans up favorites after disabling sync

--- Sources/Bookmarks/BookmarkUtils.swift
+++ Sources/Bookmarks/BookmarkUtils.swift
@@ -84,8 +84,8 @@ public struct BookmarkUtils {
         }
     }
 
-    public static func migrateToFormFactorSpecificFavorites(byCopyingExistingTo platform: FavoritesFolderID, in context: NSManagedObjectContext) {
-        assert(platform != .unified, "You must specify either desktop or mobile platform")
+    public static func migrateToFormFactorSpecificFavorites(byCopyingExistingTo folderID: FavoritesFolderID, in context: NSManagedObjectContext) {
+        assert(folderID != .unified, "You must specify either desktop or mobile folder")
 
         guard let favoritesFolder = BookmarkUtils.fetchFavoritesFolder(withUUID: FavoritesFolderID.unified.rawValue, in: context) else {
             return
@@ -94,9 +94,9 @@ public struct BookmarkUtils {
         if BookmarkUtils.fetchFavoritesFolder(withUUID: FavoritesFolderID.desktop.rawValue, in: context) == nil {
             let desktopFavoritesFolder = insertRootFolder(uuid: FavoritesFolderID.desktop.rawValue, into: context)
 
-            if platform == .desktop {
+            if folderID == .desktop {
                 favoritesFolder.favoritesArray.forEach { bookmark in
-                    bookmark.addToFavorites(folders: [desktopFavoritesFolder])
+                    bookmark.addToFavorites(favoritesRoot: desktopFavoritesFolder)
                 }
             } else {
                 desktopFavoritesFolder.shouldManageModifiedAt = false
@@ -106,16 +106,49 @@ public struct BookmarkUtils {
         if BookmarkUtils.fetchFavoritesFolder(withUUID: FavoritesFolderID.mobile.rawValue, in: context) == nil {
             let mobileFavoritesFolder = insertRootFolder(uuid: FavoritesFolderID.mobile.rawValue, into: context)
 
-            if platform == .mobile {
+            if folderID == .mobile {
                 favoritesFolder.favoritesArray.forEach { bookmark in
-                    bookmark.addToFavorites(folders: [mobileFavoritesFolder])
+                    bookmark.addToFavorites(favoritesRoot: mobileFavoritesFolder)
                 }
             } else {
                 mobileFavoritesFolder.shouldManageModifiedAt = false
             }
         }
     }
-    
+
+    public static func copyFavorites(
+        from sourceFolderID: FavoritesFolderID,
+        to targetFolderID: FavoritesFolderID,
+        removingNonNativeFavoritesFrom nonNativeFolderID: FavoritesFolderID,
+        in context: NSManagedObjectContext
+    ) {
+        assert(nonNativeFolderID != .unified, "You must specify either desktop or mobile folder")
+        assert(Set([sourceFolderID, targetFolderID, nonNativeFolderID]).count == 3, "You must pass 3 different folder IDs to this function")
+        assert([sourceFolderID, targetFolderID].contains(FavoritesFolderID.unified), "You must copy to or from a unified folder")
+
+        let allFavoritesFolders = BookmarkUtils.fetchFavoritesFolders(withUUIDs: Set(FavoritesFolderID.allCases.map(\.rawValue)), in: context)
+        assert(allFavoritesFolders.count == FavoritesFolderID.allCases.count, "Favorites folders missing")
+
+        guard let sourceFavoritesFolder = allFavoritesFolders.first(where: { $0.uuid == sourceFolderID.rawValue }),
+              let targetFavoritesFolder = allFavoritesFolders.first(where: { $0.uuid == targetFolderID.rawValue }),
+              let nonNativeFormFactorFavoritesFolder = allFavoritesFolders.first(where: { $0.uuid == nonNativeFolderID.rawValue })
+        else {
+            return
+        }
+
+        nonNativeFormFactorFavoritesFolder.favoritesArray.forEach { bookmark in
+            bookmark.removeFromFavorites(favoritesRoot: nonNativeFormFactorFavoritesFolder)
+        }
+
+        targetFavoritesFolder.favoritesArray.forEach { bookmark in
+            bookmark.removeFromFavorites(favoritesRoot: targetFavoritesFolder)
+        }
+
+        sourceFavoritesFolder.favoritesArray.forEach { bookmark in
+            bookmark.addToFavorites(favoritesRoot: targetFavoritesFolder)
+        }
+    }
+
     public static func fetchBookmark(for url: URL,
                                      predicate: NSPredicate = NSPredicate(value: true),
                                      context: NSManagedObjectContext) -> BookmarkEntity? {
