e7fc890cf4cdd67b2be0072eacf8cff492b12b2a
Christopher Brind <brindy@duckduckgo.com>
2023-09-21 11:34:46 +0100

    add model function for precise addition of bookmark (#506)

--- Sources/Bookmarks/BookmarksModel.swift
+++ Sources/Bookmarks/BookmarksModel.swift
@@ -47,6 +47,9 @@ public protocol BookmarkListInteracting: BookmarkStoring {
                       toIndex: Int)
 
     func countBookmarksForDomain(_ domain: String) -> Int
+
+    func createBookmark(title: String, url: String, folder: BookmarkEntity, folderIndex: Int, favoritesFolder: BookmarkEntity?, favoritesIndex: Int?)
+
 }
 
 public protocol FavoritesListInteracting: BookmarkStoring {
