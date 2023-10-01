8350cf450ff39002d098191f4fefcbc7efb260ad
Bartek Waresiak <bartek@duckduckgo.com>
2023-10-01 12:50:10 +0200

    Allow empty string in decrypt

--- Tests/DDGSyncTests/CrypterTests.swift
+++ Tests/DDGSyncTests/CrypterTests.swift
@@ -96,6 +96,24 @@ class CrypterTests: XCTestCase {
         XCTAssertEqual(decrypted, message)
     }
 
+    func testWhenDecryptingEmptyStringThenEmptyStringIsReturned() throws {
+        let storage = SecureStorageStub()
+        let primaryKey = Data([UInt8]((0 ..< DDGSYNCCRYPTO_PRIMARY_KEY_SIZE.rawValue).map { _ in UInt8.random(in: 0 ..< UInt8.max )}))
+        let secretKey = Data([UInt8]((0 ..< DDGSYNCCRYPTO_SECRET_KEY_SIZE.rawValue).map { _ in UInt8.random(in: 0 ..< UInt8.max )}))
+        try storage.persistAccount(SyncAccount(deviceId: "deviceId",
+                                               deviceName: "deviceName",
+                                               deviceType: "deviceType",
+                                               userId: "userId",
+                                               primaryKey: primaryKey,
+                                               secretKey: secretKey,
+                                               token: "token",
+                                               state: .active))
+        let message = ""
+        let crypter = Crypter(secureStore: storage)
+
+        XCTAssertEqual(try crypter.base64DecodeAndDecrypt(message), "")
+    }
+
     func assertValidBase64(_ base64: String) {
         for c in base64 {
             XCTAssertTrue(c.isLetter || c.isNumber || ["+", "/", "="].contains(c), "\(c) not valid base64 char")
