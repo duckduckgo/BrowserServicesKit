1bccde282e59999c5922ce170063d1351e3daa77
Diego Rey Mendez <dreymendez@duckduckgo.com>
2023-11-17 00:02:06 +0100

    Fixes some swiftlint violations

--- Sources/NetworkProtection/PacketTunnelProvider.swift
+++ Sources/NetworkProtection/PacketTunnelProvider.swift
@@ -808,7 +808,7 @@ open class PacketTunnelProvider: NEPacketTunnelProvider {
             }
 
             Task {
-                if case .connected = connectionStatus  {
+                if case .connected = connectionStatus {
                     try? await updateTunnelConfiguration(environment: settings.selectedEnvironment, serverSelectionMethod: serverSelectionMethod)
                 }
                 completionHandler?(nil)
@@ -824,7 +824,7 @@ open class PacketTunnelProvider: NEPacketTunnelProvider {
             }
 
             Task {
-                if case .connected = connectionStatus  {
+                if case .connected = connectionStatus {
                     try? await updateTunnelConfiguration(serverSelectionMethod: serverSelectionMethod)
                 }
                 completionHandler?(nil)
@@ -915,7 +915,7 @@ open class PacketTunnelProvider: NEPacketTunnelProvider {
             }
 
             settings.selectedServer = .endpoint(serverName)
-            if case .connected = connectionStatus  {
+            if case .connected = connectionStatus {
                 try? await updateTunnelConfiguration(serverSelectionMethod: .preferredServer(serverName: serverName))
             }
             completionHandler?(nil)
@@ -948,7 +948,7 @@ open class PacketTunnelProvider: NEPacketTunnelProvider {
         Task {
             self.includedRoutes = includedRoutes
 
-            if case .connected = connectionStatus  {
+            if case .connected = connectionStatus {
                 try? await updateTunnelConfiguration(reassert: false)
             }
             completionHandler?(nil)
