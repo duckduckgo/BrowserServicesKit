48a5227095641ec4fab57bc54c2f2b70b8763914
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-11-30 08:27:26 +0100

    Remove duplicated code

--- Tests/SyncDataProvidersTests/Bookmarks/BookmarksProviderTests.swift
+++ Tests/SyncDataProvidersTests/Bookmarks/BookmarksProviderTests.swift
@@ -799,41 +799,4 @@ internal class BookmarksProviderTests: BookmarksProviderTestsBase {
         }
         return rootFolder
     }
-
-    // MARK: - syncDidFinish callback
-
-    func testThatSyncDidFinishCallbackReportsModifiedAndDeletedObjectIDs() async throws {
-        let context = bookmarksDatabase.makeContext(concurrencyType: .privateQueueConcurrencyType)
-
-        let bookmarkTree = BookmarkTree {
-            Bookmark("test", id: "1")
-            Bookmark("test2", id: "2")
-        }
-
-        let received: [Syncable] = [
-            .rootFolder(children: ["1", "3"]),
-            .bookmark("test1", id: "1"),
-            .bookmark("test3", id: "3"),
-            .bookmark(id: "2", isDeleted: true)
-        ]
-
-        context.performAndWait {
-            BookmarkUtils.prepareFoldersStructure(in: context)
-            bookmarkTree.createEntities(in: context)
-            try! context.save()
-        }
-
-        expectedSyncResult = .newData(modifiedIds: ["1", "3", "bookmarks_root"], deletedIds: ["2"])
-
-        try await provider.handleSyncResponse(sent: [], received: received, clientTimestamp: Date(), serverTimestamp: "1234", crypter: crypter)
-
-        context.performAndWait {
-            context.refreshAllObjects()
-            let rootFolder = BookmarkUtils.fetchRootFolder(context)!
-            assertEquivalent(rootFolder, BookmarkTree {
-                Bookmark("test1", id: "1")
-                Bookmark("test3", id: "3")
-            })
-        }
-    }
 }
