4c085113ef5aabc5059fcdb45c593490fed94cbd
Anh Do <ado@duckduckgo.com>
2023-11-30 20:50:32 -0500

    Add support for connection status debugging

--- Sources/NetworkProtection/Status/ConnectionStatus.swift
+++ Sources/NetworkProtection/Status/ConnectionStatus.swift
@@ -25,6 +25,25 @@ public enum ConnectionStatus: Codable, Equatable {
     case connected(connectedDate: Date)
     case connecting
     case reasserting
+
+    public static var `default`: ConnectionStatus = .disconnected
+
+    public var description: String {
+        switch self {
+        case .connected:
+            return "connected"
+        case .disconnected:
+            return "disconnected"
+        case .notConfigured:
+            return "not configured"
+        case .disconnecting:
+            return "disconnecting"
+        case .connecting:
+            return "connecting"
+        case .reasserting:
+            return "reasserting"
+        }
+    }
 }
 
 /// This struct represents a status change and holds the new status and a timestamp registering when
@@ -33,9 +52,9 @@ public enum ConnectionStatus: Codable, Equatable {
 /// This is useful to know whether we have processed or still need to process the status, in case the notification
 /// is sent out more than once.
 ///
-public struct ConnectionStatusChange: Codable {
-    let status: ConnectionStatus
-    let timestamp: Date
+public struct ConnectionStatusChange: Codable, Equatable {
+    public let status: ConnectionStatus
+    public let timestamp: Date
 
     public init(status: ConnectionStatus, on timestamp: Date) {
         self.status = status
