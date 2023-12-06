c7a7e16d8bf1c2bded0eeb575dbd4af224035b08
Alexey Martemyanov <mallexxx@gmail.com>
2023-12-06 14:45:10 +0600

    set swiftlint version to 0.54.0

--- Package.swift
+++ Package.swift
@@ -386,8 +386,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.52.4/SwiftLintBinary-macos.artifactbundle.zip",
-            checksum: "8a8095e6235a07d00f34a9e500e7568b359f6f66a249f36d12cd846017a8c6f5"
+            url: "https://github.com/realm/SwiftLint/releases/download/0.54.0/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "963121d6babf2bf5fd66a21ac9297e86d855cbc9d28322790646b88dceca00f1"
         ),
         .testTarget(
             name: "NetworkProtectionTests",
