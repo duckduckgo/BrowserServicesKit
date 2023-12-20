cd117b4290fe3ea43077690360a9ce986cd71711
Michal Smaga <miasma13@gmail.com>
2023-12-20 15:40:31 +0100

    Upload MetricKit crash diagnostics to crash collecting endpoing on iOS

--- Sources/Crashes/CrashCollection.swift
+++ Sources/Crashes/CrashCollection.swift
@@ -23,32 +23,81 @@ import MetricKit
 @available(iOS 13, macOS 12, *)
 public struct CrashCollection {
 
-    // Need a strong reference
-    static let collector = CrashCollector()
-
-    public static func collectCrashesAsync(completion: @escaping ([String: String]) -> Void) {
-        collector.completion = completion
-        MXMetricManager.shared.add(collector)
+    public enum Platform {
+        case iOS, macOS
     }
 
-    final class CrashCollector: NSObject, MXMetricManagerSubscriber {
+    // Need a strong reference
+    static let crashHandler = CrashHandler()
+    static let crashSender = CrashReportSender()
 
-        var completion: ([String: String]) -> Void = { _ in }
+    public static func start(platform: Platform,
+                             firePixelHandler: @escaping ([String: String]) -> Void,
+                             showPromptIfCanSendCrashReport: @escaping ( @escaping (Bool) -> Void ) -> Void) {
 
-        func didReceive(_ payloads: [MXDiagnosticPayload]) {
-            payloads
-                .compactMap { $0.crashDiagnostics }
-                .flatMap { $0 }
-                .forEach {
-                    completion([
-                        "appVersion": "\($0.applicationVersion).\($0.metaData.applicationBuildVersion)",
-                        "code": "\($0.exceptionCode ?? -1)",
-                        "type": "\($0.exceptionType ?? -1)",
-                        "signal": "\($0.signal ?? -1)"
-                    ])
+        let first = Self.firstCrash
+
+        CrashCollection.collectCrashesAsync { params in
+            var params = params
+            if first {
+                params["first"] = "1"
+            }
+            firePixelHandler(params)
+
+            // Turn the flag off for next time
+            Self.firstCrash = false
+        } crashDiagnosticsPayloadHandler: { payloads in
+            showPromptIfCanSendCrashReport { canSend in
+                print("-- sendCrashReportHandler { shouldSend : \(canSend)")
+                if canSend {
+                    // send all the payloads
+                    payloads.forEach {
+                        print("-- sending payload")
+                        crashSender.send($0)
+                    }
                 }
+            }
+        }
+    }
+
+    private static func collectCrashesAsync(firePixel: @escaping ([String: String]) -> Void,
+                                            crashDiagnosticsPayloadHandler: @escaping ([MXDiagnosticPayload]) -> Void) {
+
+        // TODO: Remove, only for testing
+
+        class MockPayload: MXDiagnosticPayload {
+            var mockCrashes: [MXCrashDiagnostic]?
+
+            init(mockCrashes: [MXCrashDiagnostic]?) {
+                self.mockCrashes = mockCrashes
+                super.init()
+            }
+
+            required init?(coder: NSCoder) {
+                fatalError("init(coder:) has not been implemented")
+            }
+
+            override var crashDiagnostics: [MXCrashDiagnostic]? {
+                return mockCrashes
+            }
         }
 
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            let fakePayloads = [MockPayload(mockCrashes: nil),
+                                MockPayload(mockCrashes: []),
+                                MockPayload(mockCrashes: [MXCrashDiagnostic()]),
+                                MockPayload(mockCrashes: [MXCrashDiagnostic(), MXCrashDiagnostic()])]
+
+            crashHandler.didReceive(fakePayloads)
+        }
+        ////////////////////////////////////////////
+
+        
+        crashHandler.firePixelHandler = firePixel
+        crashHandler.crashDiagnosticsPayloadHandler = crashDiagnosticsPayloadHandler
+
+        MXMetricManager.shared.add(crashHandler)
     }
 
     static let firstCrashKey = "CrashCollection.first"
@@ -62,18 +111,6 @@ public struct CrashCollection {
             UserDefaults().set(newValue, forKey: Self.firstCrashKey)
         }
     }
-
-    public static func start(firePixel: @escaping ([String: String]) -> Void) {
-        let first = Self.firstCrash
-        CrashCollection.collectCrashesAsync { params in
-            var params = params
-            if first {
-                params["first"] = "1"
-            }
-            firePixel(params)
-        }
-        // Turn the flag off for next time
-        Self.firstCrash = false
-    }
-
 }
+
+
