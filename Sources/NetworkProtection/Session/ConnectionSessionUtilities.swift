621c9696cbb96b568e418569e82ea48f7b44c377
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-02-07 19:00:19 +0100

    Code cleanup

--- Sources/NetworkProtection/Session/ConnectionSessionUtilities.swift
+++ Sources/NetworkProtection/Session/ConnectionSessionUtilities.swift
@@ -22,41 +22,6 @@ import NetworkExtension
 /// These are only usable from the App that owns the tunnel.
 ///
 public class ConnectionSessionUtilities {
-    /*
-    public static func activeSession(networkExtensionBundleID: String) async throws -> NETunnelProviderSession? {
-        let managers = try await NETunnelProviderManager.loadAllFromPreferences()
-
-        guard let manager = managers.first(where: {
-            ($0.protocolConfiguration as? NETunnelProviderProtocol)?.providerBundleIdentifier == networkExtensionBundleID
-        }) else {
-            // No active connection, this is acceptable
-            return nil
-        }
-
-        guard let session = manager.connection as? NETunnelProviderSession else {
-            // The active connection is not running, so there's no session, this is acceptable
-            return nil
-        }
-
-        return session
-    }
-
-    public static func activeSession() async throws -> NETunnelProviderSession? {
-        let managers = try await NETunnelProviderManager.loadAllFromPreferences()
-
-        guard let manager = managers.first else {
-            // No active connection, this is acceptable
-            return nil
-        }
-
-        guard let session = manager.connection as? NETunnelProviderSession else {
-            // The active connection is not running, so there's no session, this is acceptable
-            return nil
-        }
-
-        return session
-    }*/
-
     /// Retrieves a session from a `NEVPNStatusDidChange` notification.
     ///
     public static func session(from notification: Notification) -> NETunnelProviderSession? {
