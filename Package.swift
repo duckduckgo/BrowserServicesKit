4fc6bc7cecbc46c18d6a6e5abe03dbda55d994d7
Alexey Martemyanov <mallexxx@gmail.com>
2024-02-07 15:36:40 +0600

    CSS -> 4.61.0

--- Package.swift
+++ Package.swift
@@ -40,7 +40,7 @@ let package = Package(
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "2.1.0"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "3.2.0"),
-        .package(url: "https://github.com/duckduckgo/content-scope-scripts", revision: "7a2476ff03486d6f1c3ec5815a2c75e48a790a30"),
+        .package(url: "https://github.com/duckduckgo/content-scope-scripts", exact: "4.61.0"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
         .package(url: "https://github.com/duckduckgo/bloom_cpp.git", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/wireguard-apple", exact: "1.1.1"),
