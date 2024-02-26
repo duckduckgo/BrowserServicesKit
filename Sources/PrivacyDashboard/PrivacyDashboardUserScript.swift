f9bd2d9f7ac922ff9b3cd7c780af3929780ddc01
Alexey Martemyanov <mallexxx@gmail.com>
2024-02-26 16:35:58 +0600

    fix build under Xcode 15.3 (#676)
    Task URL: https://app.asana.com/0/1199230911884351/1206685505427048/f

--- Sources/PrivacyDashboard/PrivacyDashboardUserScript.swift
+++ Sources/PrivacyDashboard/PrivacyDashboardUserScript.swift
@@ -69,6 +69,7 @@ final class PrivacyDashboardUserScript: NSObject, StaticUserScript {
 
     static var injectionTime: WKUserScriptInjectionTime { .atDocumentStart }
     static var forMainFrameOnly: Bool { false }
+    @MainActor
     static var source: String = ""
     static var script: WKUserScript = PrivacyDashboardUserScript.makeWKUserScript()
     var messageNames: [String] { MessageNames.allCases.map(\.rawValue) }
