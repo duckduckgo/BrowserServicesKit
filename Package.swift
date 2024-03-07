b9362c737ec0b6d09d49e6d29a6c9fc89a73088e
Alexey Martemyanov <mallexxx@gmail.com>
2024-03-07 11:56:41 +0600

    cleanup CompilerPluginSupport

--- Package.swift
+++ Package.swift
@@ -1,7 +1,6 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import CompilerPluginSupport
 import Foundation
 import PackageDescription
 
