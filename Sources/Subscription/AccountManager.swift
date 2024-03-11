4042a8e04396584566df24fb90c7b529bbe1c661
Daniel Bernal <dbernal@duckduckgo.com>
2024-03-11 23:05:53 +0100

    Subscriptions: Implement Caching for Subscription Info (#710)
    Task/Issue URL: https://app.asana.com/0/0/1206800657723184/f
    iOS PR: duckduckgo/iOS#2569
    macOS PR: duckduckgo/macos-browser#2368
    What kind of version bump will this require?: Minor
    
    Description:
    
    Implements basic caching for Subscription info
    Maintains current static approach for SubscriptionService
    Adds proper UserDefault keys to avoid conflicts
    Makes AppGroup optional in UserDefaultsCache
    Adds method to reset cache in SubscriptionService and call from Signout

--- Sources/Subscription/AccountManager.swift
+++ Sources/Subscription/AccountManager.swift
@@ -178,6 +178,7 @@ public class AccountManager: AccountManaging {
         do {
             try storage.clearAuthenticationState()
             try accessTokenStorage.removeAccessToken()
+            SubscriptionService.signOut()
             entitlementsCache.reset()
         } catch {
             if let error = error as? AccountKeychainAccessError {
@@ -219,8 +220,8 @@ public class AccountManager: AccountManaging {
         case noCachedData
     }
 
-    public func hasEntitlement(for entitlement: Entitlement.ProductName) async -> Result<Bool, Error> {
-        switch await fetchEntitlements() {
+    public func hasEntitlement(for entitlement: Entitlement.ProductName, cachePolicy: CachePolicy = .returnCacheDataElseLoad) async -> Result<Bool, Error> {
+        switch await fetchEntitlements(cachePolicy: cachePolicy) {
         case .success(let entitlements):
             return .success(entitlements.compactMap { $0.product }.contains(entitlement))
         case .failure(let error):
@@ -251,9 +252,9 @@ public class AccountManager: AccountManaging {
         }
     }
 
-    public func fetchEntitlements(policy: CachePolicy = .returnCacheDataElseLoad) async -> Result<[Entitlement], Error> {
+    public func fetchEntitlements(cachePolicy: CachePolicy = .returnCacheDataElseLoad) async -> Result<[Entitlement], Error> {
 
-        switch policy {
+        switch cachePolicy {
         case .reloadIgnoringLocalCacheData:
             return await fetchRemoteEntitlements()
 
