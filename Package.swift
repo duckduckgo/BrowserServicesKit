0449e6d81e8c04679303f660707faaaf365629a6
GioSensation <1828326+GioSensation@users.noreply.github.com>
2024-03-14 08:19:11 +0000

    Update autofill to 10.2.0

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         .library(name: "History", targets: ["History"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "10.1.0"),
+        .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "10.2.0"),
         .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.3.0"),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "1.2.2"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
