32b0411ec7392c8eb2c7896e89b04f8d4ce7db5a
Brad Slayter <slayterdevelopment@gmail.com>
2024-03-27 08:46:37 -0500

    Fix param typo and drop vpnOn from toggle Report

--- Package.swift
+++ Package.swift
@@ -43,7 +43,7 @@ let package = Package(
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "1.2.2"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "2.1.0"),
-        .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "3.3.0"),
+        .package(url: "https://github.com/duckduckgo/privacy-dashboard", revision: "b897b7153a7c55dc2c6b48bc5a3b35776ac1ffc2"),
         .package(url: "https://github.com/duckduckgo/content-scope-scripts", exact: "5.8.0"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
         .package(url: "https://github.com/duckduckgo/bloom_cpp.git", exact: "3.0.0"),
