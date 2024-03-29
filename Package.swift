ea342e2dcfbcf5bdd26d0ea3cac4cf715b2ea0a9
Sabrina Tardio <stardio@duckduckgo.com>
2024-03-29 17:47:51 +0100

    point at ssl test branch

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "2.1.0"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "3.3.0"),
-        .package(url: "https://github.com/duckduckgo/content-scope-scripts", exact: "5.7.0"),
+        .package(url: "https://github.com/duckduckgo/content-scope-scripts", branch: "03-28-built_assets_for_sslerror"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
         .package(url: "https://github.com/duckduckgo/bloom_cpp.git", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/wireguard-apple", exact: "1.1.3"),
