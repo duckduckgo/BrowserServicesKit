0eb29c8b06363d3231af873b6443f404050bd04b
Sabrina Tardio <stardio@duckduckgo.com>
2024-04-23 12:17:27 +0200

    fix lint issue

--- Sources/Common/Extensions/StringExtension.swift
+++ Sources/Common/Extensions/StringExtension.swift
@@ -102,7 +102,7 @@ public extension String {
     var isValidEmail: Bool {
         print(self)
         let emailRegEx = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: self)
     }
 
