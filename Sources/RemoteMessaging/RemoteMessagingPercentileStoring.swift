6568d48c381042275c1936c6301a5100dea1c278
Sam Symons <sam@samsymons.com>
2024-05-14 15:53:52 -0700

    Add support for `targetPercentile` RMF rule parameter (#809)
    Required:
    
    Task/Issue URL: https://app.asana.com/0/0/1207234800675204/f
    iOS PR: duckduckgo/iOS#2824
    macOS PR: duckduckgo/macos-browser#2772
    What kind of version bump will this require?: Major
    
    Description:
    
    This PR adds support for the new targetPercentile option in RMF rules. This allows us to specify a percentage of users for whom the rule matches.

---
+++ Sources/RemoteMessaging/RemoteMessagingPercentileStoring.swift
@@ -0,0 +1,51 @@
+//
+//  RemoteMessagingPercentileStoring.swift
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+public protocol RemoteMessagingPercentileStoring {
+    func percentile(forMessageId: String) -> Float
+}
+
+public class RemoteMessagingPercentileUserDefaultsStore: RemoteMessagingPercentileStoring {
+
+    enum Constants {
+        static let remoteMessagingPercentileMapping = "com.duckduckgo.app.remoteMessagingPercentileMapping"
+    }
+
+    private let userDefaults: UserDefaults
+
+    public init(userDefaults: UserDefaults) {
+        self.userDefaults = userDefaults
+    }
+
+    public func percentile(forMessageId messageID: String) -> Float {
+        var percentileMapping = (userDefaults.dictionary(forKey: Constants.remoteMessagingPercentileMapping) as? [String: Float]) ?? [:]
+
+        if let percentile = percentileMapping[messageID] {
+            return percentile
+        } else {
+            let newPercentile = Float.random(in: 0...1)
+            percentileMapping[messageID] = newPercentile
+            userDefaults.set(percentileMapping, forKey: Constants.remoteMessagingPercentileMapping)
+
+            return newPercentile
+        }
+    }
+
+}
