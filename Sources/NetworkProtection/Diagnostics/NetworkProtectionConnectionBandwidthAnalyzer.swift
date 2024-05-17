d3bd44976f6252e3f0276343116bb02ec22fdad8
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-05-17 11:10:14 +0200

    Adds some tests to validate we no longer have division by zero crashes

--- Sources/NetworkProtection/Diagnostics/NetworkProtectionConnectionBandwidthAnalyzer.swift
+++ Sources/NetworkProtection/Diagnostics/NetworkProtectionConnectionBandwidthAnalyzer.swift
@@ -24,7 +24,7 @@ import Common
 /// This class was designed to be easy to modify to eventually handle more than two snapshots over time.
 ///
 final class NetworkProtectionConnectionBandwidthAnalyzer {
-    private struct Snapshot {
+    struct Snapshot {
         let rxBytes: UInt64
         let txBytes: UInt64
         let date: Date
@@ -86,7 +86,7 @@ final class NetworkProtectionConnectionBandwidthAnalyzer {
             return
         }
 
-        let (rx, tx) = bytesPerSecond(newer: newer, older: older)
+        let (rx, tx) = Self.bytesPerSecond(newer: newer, older: older)
 
         os_log("Bytes per second in last time-interval: (rx: %{public}@, tx: %{public}@)",
                log: .networkProtectionBandwidthAnalysis,
@@ -109,8 +109,8 @@ final class NetworkProtectionConnectionBandwidthAnalyzer {
 
     // MARK: - Delta Calculation
 
-    private func bytesPerSecond(newer: Snapshot, older: Snapshot) -> (rx: Double, tx: Double) {
-        let deltaSeconds = newer.date.timeIntervalSince1970 - older.date.timeIntervalSince1970
+    static func bytesPerSecond(newer: Snapshot, older: Snapshot) -> (rx: Double, tx: Double) {
+        let deltaSeconds = newer.date.timeIntervalSince(older.date)
         let rx: Double
         let tx: Double
 
