9ec881380f8d8421e4d7f8e3791b43108a213a3a
ladamski@duckduckgo.com <ladamski@users.noreply.github.com>
2024-05-16 20:37:27 -0700

    update TrackerRadarKit to production release

--- Package.swift
+++ Package.swift
@@ -40,7 +40,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "11.0.1"),
         .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.3.0"),
-        .package(url: "https://github.com/ladamski/TrackerRadarKit", branch: "la/support-ctl-action"),
+        .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "2.1.0"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "2.1.0"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "3.5.0"),
