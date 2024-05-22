8270d631555da3a2634f37830a3629a1bd67339f
Thomas Espach <tespach@duckduckgo.com>
2024-05-22 19:51:23 +0100

    Move PhishingDetectionManager to BSK

---
+++ Tests/PhishingDetectionTests/PhishingDetectionManagerTests.swift
@@ -0,0 +1,52 @@
+//
+//  PhishingDetectionManagerTests.swift
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import XCTest
+import PhishingDetection
+@testable import DuckDuckGo_Privacy_Browser
+
+class PhishingDetectionManagerTests: XCTestCase {
+
+    public var phishingDetectionManager: MockPhishingDetectionManager!
+
+    override func setUp() {
+        super.setUp()
+        phishingDetectionManager = MockPhishingDetectionManager()
+    }
+
+    override func tearDown() {
+        phishingDetectionManager = nil
+        super.tearDown()
+    }
+
+    func testIsMalicious() async {
+        let maliciousURL = URL(string: "https://malicious.com")!
+        let nonMaliciousURL = URL(string: "https://nonmalicious.com")!
+
+        let shouldBeMalicious = await phishingDetectionManager.isMalicious(url: maliciousURL)
+        XCTAssertTrue(shouldBeMalicious)
+        let shouldNotBeMalicious = await phishingDetectionManager.isMalicious(url: nonMaliciousURL)
+        XCTAssertFalse(shouldNotBeMalicious)
+    }
+
+    func testLoadDataAsync() {
+        phishingDetectionManager.loadDataAsync()
+        XCTAssertTrue(phishingDetectionManager.didLoadData)
+    }
+}
