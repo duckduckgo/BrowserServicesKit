e9e239fe5dfeab87dcacbf5a31c8f555623a4ce2
Lucas Adamski <ladamski@users.noreply.github.com>
2024-06-14 07:33:21 -0700

    BSK support for FB CTL update (#760)
    * BSK support for FB CTL update
    
    ---------
    
    Co-authored-by: Jacek Łyp <jlyp@duckduckgo.com>
    Co-authored-by: Bartek Waresiak <bartek@duckduckgo.com>

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "11.0.2"),
         .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.3.0"),
-        .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "2.1.1"),
+        .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "2.1.2"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "2.1.0"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "4.0.0"),
