bb3f3b0fff70b32f79e940fde71bfabad21ddf1d
Anh Do <ado@duckduckgo.com>
2024-06-19 16:47:26 -0400

    Fix Swiftlint

--- Sources/NetworkProtection/PacketTunnelProvider.swift
+++ Sources/NetworkProtection/PacketTunnelProvider.swift
@@ -1004,7 +1004,7 @@ open class PacketTunnelProvider: NEPacketTunnelProvider {
         settings.apply(change: change)
     }
 
-    // swiftlint:disable:next cyclomatic_complexity
+    // swiftlint:disable:next cyclomatic_complexity function_body_length
     private func handleSettingsChange(_ change: VPNSettings.Change, completionHandler: ((Data?) -> Void)? = nil) {
         switch change {
         case .setExcludeLocalNetworks:
