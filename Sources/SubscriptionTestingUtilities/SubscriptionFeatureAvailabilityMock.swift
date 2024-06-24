c3d83398ca63b832b1ed49bf1e206da213802518
Federico Cappelli <fcappelli@duckduckgo.com>
2024-06-24 11:01:46 +0200

    lint

--- Sources/SubscriptionTestingUtilities/SubscriptionFeatureAvailabilityMock.swift
+++ Sources/SubscriptionTestingUtilities/SubscriptionFeatureAvailabilityMock.swift
@@ -1,6 +1,8 @@
 //
 //  SubscriptionFeatureAvailabilityMock.swift
 //
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
