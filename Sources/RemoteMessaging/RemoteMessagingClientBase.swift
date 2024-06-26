8e84c28896e2a060a7b4d80c6fee3a54a781ec15
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-06-26 17:13:04 +0200

    Update data source

--- Sources/RemoteMessaging/RemoteMessagingClientBase.swift
+++ Sources/RemoteMessaging/RemoteMessagingClientBase.swift
@@ -1,6 +1,5 @@
 //
 //  RemoteMessagingClientBase.swift
-//  DuckDuckGo
 //
 //  Copyright © 2024 DuckDuckGo. All rights reserved.
 //
@@ -22,30 +21,7 @@ import Common
 import Foundation
 
 public protocol RemoteMessagingDataSource {
-
-    var statisticsStore: StatisticsStore! { get }
-    var variantManager: VariantManager! { get }
-    var surveyActionMapper: RemoteMessagingSurveyActionMapping! { get }
-
-    var appTheme: String { get }
-    var isInternalUser: Bool { get }
-
-    var bookmarksCount: Int { get }
-    var favoritesCount: Int { get }
-
-    var daysSinceNetworkProtectionEnabled: Int { get }
-
-    var isWidgetInstalled: Bool { get }
-    var isPrivacyProEligibleUser: Bool { get }
-    var isPrivacyProSubscriber: Bool { get }
-    var privacyProDaysSinceSubscribed: Int { get }
-    var privacyProDaysUntilExpiry: Int { get }
-    var privacyProPurchasePlatform: String? { get }
-    var isPrivacyProSubscriptionActive: Bool { get }
-    var isPrivacyProSubscriptionExpiring: Bool { get }
-    var isPrivacyProSubscriptionExpired: Bool { get }
-
-    func refreshState() async
+    func refreshConfigMatcher(with store: RemoteMessagingStoring) async -> RemoteMessagingConfigMatcher
 }
 
 open class RemoteMessagingClientBase: RemoteMessagingFetching {
@@ -65,34 +41,7 @@ open class RemoteMessagingClientBase: RemoteMessagingFetching {
 
             os_log("Successfully fetched remote messages", log: .remoteMessaging, type: .debug)
 
-            await dataSource.refreshState()
-
-            let dismissedMessageIds = remoteMessagingStore.fetchDismissedRemoteMessageIds()
-
-            let remoteMessagingConfigMatcher = RemoteMessagingConfigMatcher(
-                appAttributeMatcher: AppAttributeMatcher(statisticsStore: dataSource.statisticsStore,
-                                                         variantManager: dataSource.variantManager,
-                                                         isInternalUser: dataSource.isInternalUser),
-                userAttributeMatcher: UserAttributeMatcher(statisticsStore: dataSource.statisticsStore,
-                                                           variantManager: dataSource.variantManager,
-                                                           bookmarksCount: dataSource.bookmarksCount,
-                                                           favoritesCount: dataSource.favoritesCount,
-                                                           appTheme: dataSource.appTheme,
-                                                           isWidgetInstalled: dataSource.isWidgetInstalled,
-                                                           daysSinceNetPEnabled: dataSource.daysSinceNetworkProtectionEnabled,
-                                                           isPrivacyProEligibleUser: dataSource.isPrivacyProEligibleUser,
-                                                           isPrivacyProSubscriber: dataSource.isPrivacyProSubscriber,
-                                                           privacyProDaysSinceSubscribed: dataSource.privacyProDaysSinceSubscribed,
-                                                           privacyProDaysUntilExpiry: dataSource.privacyProDaysUntilExpiry,
-                                                           privacyProPurchasePlatform: dataSource.privacyProPurchasePlatform,
-                                                           isPrivacyProSubscriptionActive: dataSource.isPrivacyProSubscriptionActive,
-                                                           isPrivacyProSubscriptionExpiring: dataSource.isPrivacyProSubscriptionExpiring,
-                                                           isPrivacyProSubscriptionExpired: dataSource.isPrivacyProSubscriptionExpired,
-                                                           dismissedMessageIds: dismissedMessageIds),
-                percentileStore: RemoteMessagingPercentileUserDefaultsStore(userDefaults: .standard),
-                surveyActionMapper: dataSource.surveyActionMapper,
-                dismissedMessageIds: dismissedMessageIds
-            )
+            let remoteMessagingConfigMatcher = await dataSource.refreshConfigMatcher(with: remoteMessagingStore)
 
             let processor = RemoteMessagingConfigProcessor(remoteMessagingConfigMatcher: remoteMessagingConfigMatcher)
             let config = remoteMessagingStore.fetchRemoteMessagingConfig()
