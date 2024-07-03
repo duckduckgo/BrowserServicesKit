706bb285ed6ed6bca49adf118b21b2065b83d38a
Alexey Martemyanov <mallexxx@gmail.com>
2024-07-03 19:04:56 +0600

    fix failed to restore protection always logged

--- Sources/Crashes/Mach-O/UnsafeBufferPointer+unprotected.swift
+++ Sources/Crashes/Mach-O/UnsafeBufferPointer+unprotected.swift
@@ -41,7 +41,9 @@ extension UnsafeBufferPointer {
             // restore original memory protection
             if protection & (PROT_WRITE | PROT_READ) != (PROT_WRITE | PROT_READ) {
                 let result = mprotect(mutableBuffer.baseAddress!, mutableBuffer.count * MemoryLayout<Element>.size, protection)
-                os_log(.error, "failed to restore protection %d for %s with %d", protection, self.debugDescription, result)
+                if result != 0 {
+                    os_log(.error, "failed to restore protection %d for %s with %d", protection, self.debugDescription, result)
+                }
             }
         }
 
