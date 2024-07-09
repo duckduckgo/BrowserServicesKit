3db9c9d05c87feaff27da50ffb1a3018c0a63d81
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-07-09 21:37:25 +0200

    Add unit tests for IsInstalledMacAppStore

---
+++ Tests/RemoteMessagingTests/Matchers/DesktopUserAttributeMatcherTests.swift
@@ -0,0 +1,99 @@
+//
+//  DesktopUserAttributeMatcherTests.swift
+//  DuckDuckGo
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import BrowserServicesKit
+import BrowserServicesKitTestsUtils
+import Foundation
+import RemoteMessagingTestsUtils
+import XCTest
+@testable import RemoteMessaging
+
+class DesktopUserAttributeMatcherTests: XCTestCase {
+
+    var mockStatisticsStore: MockStatisticsStore!
+    var manager: MockVariantManager!
+    var emailManager: EmailManager!
+    var matcher: DesktopUserAttributeMatcher!
+    var dateYesterday: Date!
+
+    override func setUpWithError() throws {
+        let now = Calendar.current.dateComponents(in: .current, from: Date())
+        let yesterday = DateComponents(year: now.year, month: now.month, day: now.day! - 1)
+        let dateYesterday = Calendar.current.date(from: yesterday)!
+
+        mockStatisticsStore = MockStatisticsStore()
+        mockStatisticsStore.atb = "v105-2"
+        mockStatisticsStore.appRetentionAtb = "v105-44"
+        mockStatisticsStore.searchRetentionAtb = "v105-88"
+        mockStatisticsStore.installDate = dateYesterday
+
+        manager = MockVariantManager(isSupportedReturns: true,
+                                         currentVariant: MockVariant(name: "zo", weight: 44, isIncluded: { return true }, features: [.dummy]))
+        let emailManagerStorage = MockEmailManagerStorage()
+
+        // EmailEnabledMatchingAttribute isSignedIn = true
+        emailManagerStorage.mockUsername = "username"
+        emailManagerStorage.mockToken = "token"
+
+        emailManager = EmailManager(storage: emailManagerStorage)
+        setUpUserAttributeMatcher()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+
+        matcher = nil
+    }
+
+    // MARK: - InstalledMacAppStore
+
+    func testWhenInstalledMacAppStoreMatchesThenReturnMatch() throws {
+        XCTAssertEqual(matcher.evaluate(matchingAttribute: IsInstalledMacAppStoreMatchingAttribute(value: false, fallback: nil)),
+                       .match)
+    }
+
+    func testWhenWidgetAddedDoesNotMatchThenReturnFail() throws {
+        XCTAssertEqual(matcher.evaluate(matchingAttribute: IsInstalledMacAppStoreMatchingAttribute(value: true, fallback: nil)),
+                       .fail)
+    }
+
+    // MARK: -
+
+    private func setUpUserAttributeMatcher(dismissedMessageIds: [String] = []) {
+        matcher = DesktopUserAttributeMatcher(
+            statisticsStore: mockStatisticsStore,
+            variantManager: manager,
+            emailManager: emailManager,
+            bookmarksCount: 44,
+            favoritesCount: 88,
+            appTheme: "default",
+            daysSinceNetPEnabled: 3,
+            isPrivacyProEligibleUser: true,
+            isPrivacyProSubscriber: true,
+            privacyProDaysSinceSubscribed: 5,
+            privacyProDaysUntilExpiry: 25,
+            privacyProPurchasePlatform: "apple",
+            isPrivacyProSubscriptionActive: true,
+            isPrivacyProSubscriptionExpiring: false,
+            isPrivacyProSubscriptionExpired: false,
+            dismissedMessageIds: dismissedMessageIds,
+            isInstalledMacAppStore: false
+        )
+    }
+}
