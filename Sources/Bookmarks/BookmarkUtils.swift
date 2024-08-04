92ecebfb4172ab9561959a07d7ef7037aea8c6e1
bwaresiak <bartek@duckduckgo.com>
2024-08-05 01:48:16 +0200

    Tweaks to DB API, improved abstractions for better testability. (#913)
    Required:
    
    Task/Issue URL: https://app.asana.com/0/856498667320406/1207875839908777/f
    iOS PR: duckduckgo/iOS#3143
    macOS PR: duckduckgo/macos-browser#3033
    What kind of version bump will this require?: Major
    
    Description:
    
    Tweak code to improve error handling and events propagation in Clients. Add protocols for improved testability.

--- Sources/Bookmarks/BookmarkUtils.swift
+++ Sources/Bookmarks/BookmarkUtils.swift
@@ -25,9 +25,11 @@ public struct BookmarkUtils {
         let request = BookmarkEntity.fetchRequest()
         request.predicate = NSPredicate(format: "%K == %@", #keyPath(BookmarkEntity.uuid), BookmarkEntity.Constants.rootFolderID)
         request.returnsObjectsAsFaults = false
-        request.fetchLimit = 1
 
-        return try? context.fetch(request).first
+        let result = (try? context.fetch(request)) ?? []
+
+        // We cannot use simply sort descriptor as this is to-many on both sides of a relationship.
+        return result.sorted(by: { ($0.children?.count ?? 0) > ($1.children?.count ?? 0) }).first
     }
 
     public static func fetchFavoritesFolders(for displayMode: FavoritesDisplayMode, in context: NSManagedObjectContext) -> [BookmarkEntity] {
@@ -40,9 +42,11 @@ public struct BookmarkUtils {
         let request = BookmarkEntity.fetchRequest()
         request.predicate = NSPredicate(format: "%K == %@", #keyPath(BookmarkEntity.uuid), uuid)
         request.returnsObjectsAsFaults = false
-        request.fetchLimit = 1
 
-        return try? context.fetch(request).first
+        let result = (try? context.fetch(request)) ?? []
+
+        // We cannot use simply sort descriptor as this is to-many on both sides of a relationship.
+        return result.sorted(by: { ($0.favorites?.count ?? 0) > ($1.favorites?.count ?? 0) }).first
     }
 
     public static func fetchFavoritesFolders(withUUIDs uuids: Set<String>, in context: NSManagedObjectContext) -> [BookmarkEntity] {
@@ -51,9 +55,18 @@ public struct BookmarkUtils {
         let request = BookmarkEntity.fetchRequest()
         request.predicate = NSPredicate(format: "%K in %@", #keyPath(BookmarkEntity.uuid), uuids)
         request.returnsObjectsAsFaults = false
-        request.fetchLimit = uuids.count
 
-        return (try? context.fetch(request)) ?? []
+        var objects = (try? context.fetch(request)) ?? []
+        objects.sort(by: { ($0.favorites?.count ?? 0) > ($1.favorites?.count ?? 0) })
+
+        var result = [BookmarkEntity]()
+        for uuid in uuids {
+            if let entity = objects.first(where: { $0.uuid == uuid }) {
+                result.append(entity)
+            }
+        }
+
+        return result
     }
 
     public static func fetchOrphanedEntities(_ context: NSManagedObjectContext) -> [BookmarkEntity] {
@@ -227,15 +240,22 @@ public struct BookmarkUtils {
 
 extension BookmarkUtils {
 
-    public static func prepareLegacyFoldersStructure(in context: NSManagedObjectContext) {
+    public static func prepareLegacyFoldersStructure(in context: NSManagedObjectContext) throws {
 
-        if fetchRootFolder(context) == nil {
-            insertRootFolder(uuid: BookmarkEntity.Constants.rootFolderID, into: context)
+        func prepareRootFolder(uuid: String) throws {
+            let request = BookmarkEntity.fetchRequest()
+            request.predicate = NSPredicate(format: "%K == %@", #keyPath(BookmarkEntity.uuid), uuid)
+            request.returnsObjectsAsFaults = false
+            request.fetchLimit = 1
+
+            let root = try context.fetch(request).first
+            if root == nil {
+                insertRootFolder(uuid: uuid, into: context)
+            }
         }
 
-        if fetchLegacyFavoritesFolder(context) == nil {
-            insertRootFolder(uuid: legacyFavoritesFolderID, into: context)
-        }
+        try prepareRootFolder(uuid: BookmarkEntity.Constants.rootFolderID)
+        try prepareRootFolder(uuid: legacyFavoritesFolderID)
     }
 
     public static func fetchLegacyFavoritesFolder(_ context: NSManagedObjectContext) -> BookmarkEntity? {
