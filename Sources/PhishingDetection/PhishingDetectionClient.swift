f6ede16e1b4cd4e4edfc0c32257b30ce8b33eb98
Thom Espach <thomespach@me.com>
2024-08-15 13:41:52 +0100

    Phishing Detection Data Updates and Mocks (#933)
    <!--
    Note: This checklist is a reminder of our shared engineering
    expectations.
    -->
    
    Please review the release process for BrowserServicesKit
    [here](https://app.asana.com/0/1200194497630846/1200837094583426).
    
    **Required**:
    
    Task/Issue URL:
    https://app.asana.com/0/1204023833050360/1207976613228506/f
    iOS PR: -
    macOS PR: -
    What kind of version bump will this require?: Major/Minor/Patch
    
    **Description**:
    Implements the service wrapper for phishing detection, including the
    backend API client, and data update manager.
    1. PhishingDetectionUpdateManager - uses the client and data store to
    implement the data update logic
    2. PhishingDetectionClient - makes web requests to the backend service
    3. PhishingDetector - exposes isMalicious to client app
    
    <!--
    Tagging instructions
    If this PR isn't ready to be merged for whatever reason it should be
    marked with the `DO NOT MERGE` label (particularly if it's a draft)
    If it's pending Product Review/PFR, please add the `Pending Product
    Review` label.
    
    If at any point it isn't actively being worked on/ready for
    review/otherwise moving forward (besides the above PR/PFR exception)
    strongly consider closing it (or not opening it in the first place). If
    you decide not to close it, make sure it's labelled to make it clear the
    PRs state and comment with more information.
    -->
    
    **Steps to test this PR**:
    1. 
    
    <!--
    Before submitting a PR, please ensure you have tested the combinations
    you expect the reviewer to test, then delete configurations you *know*
    do not need explicit testing.
    
    Using a simulator where a physical device is unavailable is acceptable.
    -->
    
    **OS Testing**:
    
    * [ ] iOS 14
    * [ ] iOS 15
    * [ ] iOS 16
    * [ ] macOS 10.15
    * [ ] macOS 11
    * [ ] macOS 12
    
    ---
    ###### Internal references:
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)

---
+++ Sources/PhishingDetection/PhishingDetectionClient.swift
@@ -0,0 +1,176 @@
+//
+//  PhishingDetectionClient.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import Common
+
+public struct HashPrefixResponse: Codable, Equatable {
+    public var insert: [String]
+    public var delete: [String]
+    public var revision: Int
+    public var replace: Bool
+
+    public init(insert: [String], delete: [String], revision: Int, replace: Bool) {
+        self.insert = insert
+        self.delete = delete
+        self.revision = revision
+        self.replace = replace
+    }
+}
+
+public struct FilterSetResponse: Codable, Equatable {
+    public var insert: [Filter]
+    public var delete: [Filter]
+    public var revision: Int
+    public var replace: Bool
+
+    public init(insert: [Filter], delete: [Filter], revision: Int, replace: Bool) {
+        self.insert = insert
+        self.delete = delete
+        self.revision = revision
+        self.replace = replace
+    }
+}
+
+public struct MatchResponse: Codable, Equatable {
+    public var matches: [Match]
+}
+
+public protocol PhishingDetectionClientProtocol {
+    func getFilterSet(revision: Int) async -> FilterSetResponse
+    func getHashPrefixes(revision: Int) async -> HashPrefixResponse
+    func getMatches(hashPrefix: String) async -> [Match]
+}
+
+public protocol URLSessionProtocol {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse)
+}
+
+extension URLSession: URLSessionProtocol {}
+
+extension URLSessionProtocol {
+    public static var defaultSession: URLSessionProtocol {
+        return URLSession.shared
+    }
+}
+
+public class PhishingDetectionAPIClient: PhishingDetectionClientProtocol {
+
+    public enum Environment {
+        case production
+        case staging
+    }
+
+    enum Constants {
+        static let productionEndpoint = URL(string: "https://duckduckgo.com/api/protection/")!
+        static let stagingEndpoint = URL(string: "https://web-use-1.duckduckgo.com/api/protection/")!
+        enum APIPath: String {
+            case filterSet
+            case hashPrefix
+            case matches
+        }
+    }
+
+    private let endpointURL: URL
+    private let session: URLSessionProtocol!
+    private var headers: [String: String]? = [:]
+
+    var filterSetURL: URL {
+        endpointURL.appendingPathComponent(Constants.APIPath.filterSet.rawValue)
+    }
+
+    var hashPrefixURL: URL {
+        endpointURL.appendingPathComponent(Constants.APIPath.hashPrefix.rawValue)
+    }
+
+    var matchesURL: URL {
+        endpointURL.appendingPathComponent(Constants.APIPath.matches.rawValue)
+    }
+
+    public init(environment: Environment = .production, session: URLSessionProtocol = URLSession.defaultSession) {
+        switch environment {
+        case .production:
+            endpointURL = Constants.productionEndpoint
+        case .staging:
+            endpointURL = Constants.stagingEndpoint
+        }
+        self.session = session
+    }
+
+    public func getFilterSet(revision: Int) async -> FilterSetResponse {
+        guard let url = createURL(for: .filterSet, revision: revision) else {
+            logDebug("🔸 Invalid filterSet revision URL: \(revision)")
+            return FilterSetResponse(insert: [], delete: [], revision: revision, replace: false)
+        }
+        return await fetch(url: url, responseType: FilterSetResponse.self) ?? FilterSetResponse(insert: [], delete: [], revision: revision, replace: false)
+    }
+
+    public func getHashPrefixes(revision: Int) async -> HashPrefixResponse {
+        guard let url = createURL(for: .hashPrefix, revision: revision) else {
+            logDebug("🔸 Invalid hashPrefix revision URL: \(revision)")
+            return HashPrefixResponse(insert: [], delete: [], revision: revision, replace: false)
+        }
+        return await fetch(url: url, responseType: HashPrefixResponse.self) ?? HashPrefixResponse(insert: [], delete: [], revision: revision, replace: false)
+    }
+
+    public func getMatches(hashPrefix: String) async -> [Match] {
+        let queryItems = [URLQueryItem(name: "hashPrefix", value: hashPrefix)]
+        guard let url = createURL(for: .matches, queryItems: queryItems) else {
+            logDebug("🔸 Invalid matches URL: \(hashPrefix)")
+            return []
+        }
+        return await fetch(url: url, responseType: MatchResponse.self)?.matches ?? []
+    }
+}
+
+// MARK: Private Methods
+extension PhishingDetectionAPIClient {
+
+    private func logDebug(_ message: String) {
+        os_log(.debug, log: .phishingDetection, "\(self): \(message)")
+    }
+
+    private func createURL(for path: Constants.APIPath, revision: Int? = nil, queryItems: [URLQueryItem]? = nil) -> URL? {
+        // Start with the base URL and append the path component
+        var urlComponents = URLComponents(url: endpointURL.appendingPathComponent(path.rawValue), resolvingAgainstBaseURL: true)
+        var items = queryItems ?? []
+        if let revision = revision, revision > 0 {
+            items.append(URLQueryItem(name: "revision", value: String(revision)))
+        }
+        urlComponents?.queryItems = items.isEmpty ? nil : items
+        return urlComponents?.url
+    }
+
+    private func fetch<T: Decodable>(url: URL, responseType: T.Type) async -> T? {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+
+        do {
+            let (data, _) = try await session.data(for: request)
+            if let response = try? JSONDecoder().decode(responseType, from: data) {
+                return response
+            } else {
+                logDebug("🔸 Failed to decode response for \(String(describing: responseType)): \(data)")
+            }
+        } catch {
+            logDebug("🔴 Failed to load \(String(describing: responseType)) data: \(error)")
+        }
+        return nil
+    }
+}
---
+++ Tests/PhishingDetectionTests/PhishingDetectorTests.swift
@@ -0,0 +1,97 @@
+//
+//  PhishingDetectorTests.swift
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+import Foundation
+import XCTest
+@testable import PhishingDetection
+
+class IsMaliciousTests: XCTestCase {
+    func testIsMaliciousWithLocalFilterHit() async {
+        let mockAPIClient = MockPhishingDetectionClient()
+        let mockDataStore = MockPhishingDetectionDataStore()
+        let detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore)
+        let filter = Filter(hashValue: "255a8a793097aeea1f06a19c08cde28db0eb34c660c6e4e7480c9525d034b16d", regex: ".*malicious.*")
+        mockDataStore.filterSet = Set([filter])
+        mockDataStore.hashPrefixes = Set(["255a8a79"])
+
+        let url = URL(string: "https://malicious.com/")!
+
+        let result = await detector.isMalicious(url: url)
+
+        XCTAssertTrue(result)
+    }
+
+    func testIsMaliciousWithApiMatch() async {
+        let mockAPIClient = MockPhishingDetectionClient()
+        let mockDataStore = MockPhishingDetectionDataStore()
+        let detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore)
+        mockDataStore.filterSet = Set()
+        mockDataStore.hashPrefixes = ["a379a6f6"]
+
+        let url = URL(string: "https://example.com/mal")!
+
+        let result = await detector.isMalicious(url: url)
+
+        XCTAssertTrue(result)
+    }
+
+    func testIsMaliciousWithHashPrefixMatch() async {
+        let mockAPIClient = MockPhishingDetectionClient()
+        let mockDataStore = MockPhishingDetectionDataStore()
+        let detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore)
+        let filter = Filter(hashValue: "notamatch", regex: ".*malicious.*")
+        mockDataStore.filterSet = [filter]
+        mockDataStore.hashPrefixes = ["4c64eb24"] // matches safe.com
+
+        let url = URL(string: "https://safe.com")!
+
+        let result = await detector.isMalicious(url: url)
+
+        XCTAssertFalse(result)
+    }
+
+    func testIsMaliciousWithFullHashMatch() async {
+        // 4c64eb2468bcd3e113b37167e6b819aeccf550f974a6082ef17fb74ca68e823b
+        let mockAPIClient = MockPhishingDetectionClient()
+        let mockDataStore = MockPhishingDetectionDataStore()
+        let detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore)
+        let filter = Filter(hashValue: "4c64eb2468bcd3e113b37167e6b819aeccf550f974a6082ef17fb74ca68e823b", regex: "https://safe.com/maliciousURI")
+        mockDataStore.filterSet = [filter]
+        mockDataStore.hashPrefixes = ["4c64eb24"]
+
+        let url = URL(string: "https://safe.com")!
+
+        let result = await detector.isMalicious(url: url)
+
+        XCTAssertFalse(result)
+    }
+
+    func testIsMaliciousWithNoHashPrefixMatch() async {
+        let mockAPIClient = MockPhishingDetectionClient()
+        let mockDataStore = MockPhishingDetectionDataStore()
+        let detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore)
+        let filter = Filter(hashValue: "testHash", regex: ".*malicious.*")
+        mockDataStore.filterSet = [filter]
+        mockDataStore.hashPrefixes = ["testPrefix"]
+
+        let url = URL(string: "https://safe.com")!
+
+        let result = await detector.isMalicious(url: url)
+
+        XCTAssertFalse(result)
+    }
+}
