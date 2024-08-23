5515a061b647a6af1c886d469e4dd21825379990
Thom Espach <thomespach@me.com>
2024-08-23 13:32:20 +0100

    Add Phishing Detection Privacy Dashboard + Config (#936)
    <!--
    Note: This checklist is a reminder of our shared engineering
    expectations.
    -->
    
    Please review the release process for BrowserServicesKit
    [here](https://app.asana.com/0/1200194497630846/1200837094583426).
    
    **Required**:
    
    Task/Issue URL: https://app.asana.com/0/0/1207976613228512/f
    iOS PR: -
    macOS PR: -
    What kind of version bump will this require?: Minor
    
    **Description**:
    Functions for PrivacyInfo and PrivacyDashboard
    - Enable the isPhishing flag to trigger the phishing detected view on
    the privacy dashboard.
    - When isPhishing is set, it should raise the phishing detected view on
    the privacy dashboard.
    - Add custom case where subfeatures are enabled only for internal users
    (currently it would return disabled for internal)
    
    
    <!--
    Tagging instructions
    If this PR isn't ready to be merged for whatever reason it should be
    marked with the `DO NOT MERGE` label (particularly if it's a draft)
    If it's pending Product Review/PFR, please add the `Pending Product
    Review` label.
    
    If at any point it isn't actively being worked on/ready for
    review/otherwise moving forward (besides the above PR/PFR exception)
    strongly consider closing it (or not opening it in the first place). If
    you decide not to close it, make sure it's labelled to make it clear the
    PRs state and comment with more information.
    -->
    
    **Steps to test this PR**:
    1.
    
    <!--
    Before submitting a PR, please ensure you have tested the combinations
    you expect the reviewer to test, then delete configurations you *know*
    do not need explicit testing.
    
    Using a simulator where a physical device is unavailable is acceptable.
    -->
    
    **OS Testing**:
    
    * [ ] iOS 14
    * [ ] iOS 15
    * [ ] iOS 16
    * [ ] macOS 10.15
    * [ ] macOS 11
    * [ ] macOS 12
    
    ---
    ###### Internal references:
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    
    ---------
    
    Co-authored-by: Sabrina Tardio <stardio@duckduckgo.com>

--- Sources/BrowserServicesKit/PrivacyConfig/Features/PrivacyFeature.swift
+++ Sources/BrowserServicesKit/PrivacyConfig/Features/PrivacyFeature.swift
@@ -49,6 +49,7 @@ public enum PrivacyFeature: String {
     case sslCertificates
     case brokenSiteReportExperiment
     case toggleReports
+    case phishingDetection
     case remoteMessaging
     case additionalCampaignPixelParams
 }
@@ -115,7 +116,7 @@ public enum PrivacyProSubfeature: String, Equatable, PrivacySubfeature {
     case isLaunchedOverrideStripe
 }
 
-public enum sslCertificatesSubfeature: String, PrivacySubfeature {
+public enum SslCertificatesSubfeature: String, PrivacySubfeature {
     public var parent: PrivacyFeature { .sslCertificates }
     case allowBypass
 }
@@ -125,4 +126,10 @@ public enum DuckPlayerSubfeature: String, PrivacySubfeature {
     case pip
     case autoplay
     case openInNewTab
+}
+
+public enum PhishingDetectionSubfeature: String, PrivacySubfeature {
+    public var parent: PrivacyFeature { .phishingDetection }
+    case allowErrorPage
+    case allowPreferencesToggle
 }
