a7706452156d833114b90e31020f8df6f52717e1
Thom Espach <thomespach@me.com>
2024-08-23 13:04:15 +0100

    Remove internal user check in SubfeatureEnabled.

--- Sources/BrowserServicesKit/PrivacyConfig/AppPrivacyConfiguration.swift
+++ Sources/BrowserServicesKit/PrivacyConfig/AppPrivacyConfiguration.swift
@@ -182,10 +182,7 @@ public struct AppPrivacyConfiguration: PrivacyConfiguration {
         switch stateFor(subfeature, versionProvider: versionProvider, randomizer: randomizer) {
         case .enabled:
             return true
-        case .disabled(let reason):
-            if reason == .limitedToInternalUsers {
-                return internalUserDecider.isInternalUser
-            }
+        case .disabled:
             return false
         }
     }
