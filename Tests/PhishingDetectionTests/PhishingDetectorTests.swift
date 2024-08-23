fb65da2b3eb005bb10979321718c21dc7a4ae269
Thom Espach <thomespach@me.com>
2024-08-23 17:29:28 +0100

    Add eventMappingMock to PhishingDetectorTests

--- Tests/PhishingDetectionTests/PhishingDetectorTests.swift
+++ Tests/PhishingDetectionTests/PhishingDetectorTests.swift
@@ -20,10 +20,29 @@ import XCTest
 @testable import PhishingDetection
 
 class IsMaliciousTests: XCTestCase {
+    
+    private var mockAPIClient: MockPhishingDetectionClient!
+    private var mockDataStore: MockPhishingDetectionDataStore!
+    private var mockEventMapping: MockEventMapping!
+    private var detector: PhishingDetector!
+    
+    override func setUp() {
+        super.setUp()
+        mockAPIClient = MockPhishingDetectionClient()
+        mockDataStore = MockPhishingDetectionDataStore()
+        mockEventMapping = MockEventMapping()
+        detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore, eventMapping: mockEventMapping)
+    }
+    
+    override func tearDown() {
+        mockAPIClient = nil
+        mockDataStore = nil
+        mockEventMapping = nil
+        detector = nil
+        super.tearDown()
+    }
+    
     func testIsMaliciousWithLocalFilterHit() async {
-        let mockAPIClient = MockPhishingDetectionClient()
-        let mockDataStore = MockPhishingDetectionDataStore()
-        let detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore)
         let filter = Filter(hashValue: "255a8a793097aeea1f06a19c08cde28db0eb34c660c6e4e7480c9525d034b16d", regex: ".*malicious.*")
         mockDataStore.filterSet = Set([filter])
         mockDataStore.hashPrefixes = Set(["255a8a79"])
@@ -36,9 +55,6 @@ class IsMaliciousTests: XCTestCase {
     }
 
     func testIsMaliciousWithApiMatch() async {
-        let mockAPIClient = MockPhishingDetectionClient()
-        let mockDataStore = MockPhishingDetectionDataStore()
-        let detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore)
         mockDataStore.filterSet = Set()
         mockDataStore.hashPrefixes = ["a379a6f6"]
 
@@ -50,9 +66,6 @@ class IsMaliciousTests: XCTestCase {
     }
 
     func testIsMaliciousWithHashPrefixMatch() async {
-        let mockAPIClient = MockPhishingDetectionClient()
-        let mockDataStore = MockPhishingDetectionDataStore()
-        let detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore)
         let filter = Filter(hashValue: "notamatch", regex: ".*malicious.*")
         mockDataStore.filterSet = [filter]
         mockDataStore.hashPrefixes = ["4c64eb24"] // matches safe.com
@@ -66,9 +79,6 @@ class IsMaliciousTests: XCTestCase {
 
     func testIsMaliciousWithFullHashMatch() async {
         // 4c64eb2468bcd3e113b37167e6b819aeccf550f974a6082ef17fb74ca68e823b
-        let mockAPIClient = MockPhishingDetectionClient()
-        let mockDataStore = MockPhishingDetectionDataStore()
-        let detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore)
         let filter = Filter(hashValue: "4c64eb2468bcd3e113b37167e6b819aeccf550f974a6082ef17fb74ca68e823b", regex: "https://safe.com/maliciousURI")
         mockDataStore.filterSet = [filter]
         mockDataStore.hashPrefixes = ["4c64eb24"]
@@ -81,9 +91,6 @@ class IsMaliciousTests: XCTestCase {
     }
 
     func testIsMaliciousWithNoHashPrefixMatch() async {
-        let mockAPIClient = MockPhishingDetectionClient()
-        let mockDataStore = MockPhishingDetectionDataStore()
-        let detector = PhishingDetector(apiClient: mockAPIClient, dataStore: mockDataStore)
         let filter = Filter(hashValue: "testHash", regex: ".*malicious.*")
         mockDataStore.filterSet = [filter]
         mockDataStore.hashPrefixes = ["testPrefix"]
