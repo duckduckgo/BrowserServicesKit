64446682fdfd94a200b36da5717c7038a3302c43
Sabrina Tardio <stardio@duckduckgo.com>
2024-08-28 16:47:58 +0200

    fix lint errors

--- Tests/SpecialErrorPagesTests/SpecialErrorPagesTest.swift
+++ Tests/SpecialErrorPagesTests/SpecialErrorPagesTest.swift
@@ -1,8 +1,7 @@
 //
-//  SpecialErrorPagesTest.swift
-//  DuckDuckGo
+//  SpecialErrorPageUserScriptTests.swift
 //
-//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -182,7 +181,6 @@ final class SpecialErrorPageUserScriptTests: XCTestCase {
         XCTAssertNil(encodable)
         XCTAssertTrue(delegate.advancedInfoPresentedCalled)
     }
-
 
 }
 
