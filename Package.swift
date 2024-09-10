dac7befac719bdd1587fce0f28e24d30fe4e9c82
Graeme Arthur <2030310+graeme@users.noreply.github.com>
2024-09-10 09:26:01 +0200

    Point BSK to autofill 13.1.0

--- Package.swift
+++ Package.swift
@@ -45,7 +45,7 @@ let package = Package(
         .library(name: "Onboarding", targets: ["Onboarding"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", branch: "main"),
+        .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "13.1.0"),
         .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.4.0"),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
