cafe60012879a42fb3e4ac50af4c698eecf88fd9
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-09-19 09:03:17 +0200

    Upgrade Punycode to 3.0.0

--- Package.swift
+++ Package.swift
@@ -50,7 +50,7 @@ let package = Package(
         .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.4.0"),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
-        .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "2.1.0"),
+        .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "5.3.0"),
         .package(url: "https://github.com/duckduckgo/content-scope-scripts", exact: "6.15.0"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
@@ -194,7 +194,7 @@ let package = Package(
         .target(
             name: "Common",
             dependencies: [
-                .product(name: "Punnycode", package: "PunycodeSwift"),
+                .product(name: "Punycode", package: "PunycodeSwift"),
             ],
             resources: [
                 .process("TLD/tlds.json")
