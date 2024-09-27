93eaee2dec0c6ca36242081ca1d952e706c7547b
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2024-09-27 18:28:03 +0200

    Fix linter issues

--- Tests/BookmarksTests/BookmarksSanitizationTests.swift
+++ Tests/BookmarksTests/BookmarksSanitizationTests.swift
@@ -23,17 +23,17 @@ import CoreData
 import Foundation
 
 final class BookmarksSanitizationTests: XCTestCase {
-    
+
     private var bookmarksDatabase: CoreDataDatabase!
     private var location: URL!
     private var context: NSManagedObjectContext!
     private var rootFolder: BookmarkEntity!
-    
+
     override func setUp() {
         super.setUp()
-        
+
         location = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
-        
+
         let bundle = Bookmarks.bundle
         guard let model = CoreDataDatabase.loadModel(from: bundle, named: "BookmarksModel") else {
             XCTFail("Failed to load model")
@@ -41,88 +41,88 @@ final class BookmarksSanitizationTests: XCTestCase {
         }
         bookmarksDatabase = CoreDataDatabase(name: type(of: self).description(), containerLocation: location, model: model)
         bookmarksDatabase.loadStore()
-        
+
         context = bookmarksDatabase.makeContext(concurrencyType: .privateQueueConcurrencyType)
         context.performAndWait {
             BookmarkUtils.prepareFoldersStructure(in: context)
             try! context.save()
         }
-        
+
         rootFolder = BookmarkUtils.fetchRootFolder(context)
     }
-    
+
     override func tearDown() {
         super.tearDown()
-        
+
         try? FileManager.default.removeItem(at: location)
     }
-    
+
     func testRunsCustomSanitization() {
         let bookmark = stubBookmark(url: "some.url", in: context)
         var didRunCustomFunction = false
         let sanitization = BookmarkSanitization.custom { _ in
             didRunCustomFunction = true
         }
-        
+
         sanitization.sanitize(bookmark)
-        
+
         XCTAssertTrue(didRunCustomFunction)
     }
-    
+
     func testNavigationalSanitizationAddsSchemeToURL() {
         let bookmark = stubBookmark(url: "some.url", in: context)
         let sanitization = BookmarkSanitization.navigational
-        
+
         sanitization.sanitize(bookmark)
-        
+
         XCTAssertEqual(bookmark.url, "http://some.url")
     }
-    
+
     func testNavigationalSanitizationAllowsIPAddress() {
         let bookmark = stubBookmark(url: "192.168.1.1", in: context)
         let sanitization = BookmarkSanitization.navigational
-        
+
         sanitization.sanitize(bookmark)
-        
+
         XCTAssertEqual(bookmark.url, "http://192.168.1.1")
     }
-    
+
     func testNavigationalSanitizationKeepsExistingHTTPScheme() {
         let bookmark = stubBookmark(url: "https://some.url", in: context)
         let sanitization = BookmarkSanitization.navigational
-        
+
         sanitization.sanitize(bookmark)
-        
+
         XCTAssertEqual(bookmark.url, "https://some.url")
     }
-    
+
     func testNavigationalSanitizationKeepsExistingScheme() {
         let bookmark = stubBookmark(url: "somescheme://some.url", in: context)
         let sanitization = BookmarkSanitization.navigational
-        
+
         sanitization.sanitize(bookmark)
-        
+
         XCTAssertEqual(bookmark.url, "somescheme://some.url")
     }
-    
+
     func testNavigationalSanitizationUsesPunycodeForEmoji() {
         let bookmark = stubBookmark(url: "https://😍.url", in: context)
         let sanitization = BookmarkSanitization.navigational
-        
+
         sanitization.sanitize(bookmark)
-        
+
         XCTAssertEqual(bookmark.url, "https://xn--r28h.url")
     }
-    
+
     func testNavigationalSanitizationKeepsEmptyString() {
         let bookmark = stubBookmark(url: "  ", in: context)
         let sanitization = BookmarkSanitization.navigational
-        
+
         sanitization.sanitize(bookmark)
-        
+
         XCTAssertEqual(bookmark.url, "  ")
     }
-    
+
     private func stubBookmark(url: String, in context: NSManagedObjectContext) -> BookmarkEntity {
         BookmarkEntity.makeBookmark(title: "", url: url, parent: rootFolder, context: context)
     }
