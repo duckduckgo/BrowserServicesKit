d8460df98043222b4ef7848089ccba115c1e3de4
Anh Do <ado@duckduckgo.com>
2024-10-16 15:36:12 -0400

    Points to Marcos' css branch

--- Package.swift
+++ Package.swift
@@ -52,7 +52,7 @@ let package = Package(
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "5.3.0"),
-        .package(url: "https://github.com/duckduckgo/content-scope-scripts", exact: "6.19.0"),
+        .package(url: "https://github.com/duckduckgo/content-scope-scripts", branch: "pr-releases/pr-1105"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
         .package(url: "https://github.com/duckduckgo/bloom_cpp.git", exact: "3.0.0"),
         .package(url: "https://github.com/1024jp/GzipSwift.git", exact: "6.0.1")
