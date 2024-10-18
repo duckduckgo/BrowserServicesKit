cf4a869c4a109af1f803325da70aec2ac006ea2e
Graeme Arthur <2030310+graeme@users.noreply.github.com>
2024-10-18 17:32:21 +0200

    Bump autofill to 15.0.0

--- Package.swift
+++ Package.swift
@@ -46,7 +46,7 @@ let package = Package(
         .library(name: "Onboarding", targets: ["Onboarding"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", branch: "dbajpeyi/remove-get-available-input-types-caching"),
+        .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "15.0.0"),
         .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.4.0"),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
