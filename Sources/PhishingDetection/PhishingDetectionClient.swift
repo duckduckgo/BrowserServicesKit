eac223786df7c9e6c39c2de9220772487095a25a
Thomas Espach <tespach@duckduckgo.com>
2024-10-28 11:58:34 +0000

    Undo remove caching

--- Sources/PhishingDetection/PhishingDetectionClient.swift
+++ Sources/PhishingDetection/PhishingDetectionClient.swift
@@ -161,7 +161,6 @@ extension PhishingDetectionAPIClient {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.allHTTPHeaderFields = headers
-        request.cachePolicy = .reloadIgnoringLocalCacheData // Disable caching
 
         do {
             let (data, _) = try await session.data(for: request)
