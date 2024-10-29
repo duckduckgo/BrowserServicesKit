4dd33d2872194be0cd1cd638b4d6020c6ec51b48
Sam Symons <sam@samsymons.com>
2024-10-28 19:44:57 -0700

    Disable additional tests.

--- Tests/BrowserServicesKitTests/ContentBlocker/SurrogatesReferenceTests.swift
+++ Tests/BrowserServicesKitTests/ContentBlocker/SurrogatesReferenceTests.swift
@@ -16,6 +16,9 @@
 //  limitations under the License.
 //
 
+// Tests are disabled on iOS due to WKWebView stability issues on the iOS 17.5+ simulator.
+#if os(macOS)
+
 import XCTest
 @testable import TrackerRadarKit
 import os.log
@@ -226,3 +229,5 @@ final class SurrogatesReferenceTests: XCTestCase {
         }
     }
 }
+
+#endif
