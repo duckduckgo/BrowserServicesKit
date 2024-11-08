e89ceb7c59fcb4d803c5fd1fee6167ffb2020fa5
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-11-08 02:21:43 +0100

    Fixes a swiflint warning

--- Sources/NetworkProtection/Routing/VPNRoutingRange.swift
+++ Sources/NetworkProtection/Routing/VPNRoutingRange.swift
@@ -35,7 +35,7 @@ public enum VPNRoutingRange {
     ]
 
     public static let localNetworkRange: [NetworkProtection.IPAddressRange] = [
-        //"10.0.0.0/8",     /* 255.0.0.0   */
+        // "10.0.0.0/8",     /* 255.0.0.0   */
         "172.16.0.0/12",  /* 255.240.0.0 */
         "192.168.0.0/16", /* 255.255.0.0 */
     ]
