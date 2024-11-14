303fb2d0bcb32e08fbaa5ecd7a437064f68bc931
Sam Symons <sam@samsymons.com>
2024-11-14 14:35:31 -0800

    Set GRDB to 2.4.2.

--- Package.swift
+++ Package.swift
@@ -49,7 +49,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "15.1.0"),
-        .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.4.0"),
+        .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.4.2"),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.3.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "3.0.0"),
