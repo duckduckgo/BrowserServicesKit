4eaf35f5710607db51001acaf842812b04fa043e
Graeme Arthur <2030310+graeme@users.noreply.github.com>
2024-11-20 14:43:26 +0100

    Add multi-tab support

--- Sources/Common/Extensions/DateExtension.swift
+++ Sources/Common/Extensions/DateExtension.swift
@@ -135,4 +135,8 @@ public extension Date {
         self > Date().addingTimeInterval(Double(-days) * 24 * 60 * 60)
     }
 
+    func isLessThan(minutesAgo minutes: Int) -> Bool {
+        self > Date().addingTimeInterval(Double(-minutes) * 60)
+    }
+
 }
