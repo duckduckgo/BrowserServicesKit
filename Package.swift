dd145b68bcd90a76cc50f7e5b96819902febf927
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-12-18 15:51:22 +0100

    Update C-S-S to use a build from a branch

--- Package.swift
+++ Package.swift
@@ -55,7 +55,7 @@ let package = Package(
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.3.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "3.0.0"),
-        .package(url: "https://github.com/duckduckgo/content-scope-scripts", exact: "6.43.0"),
+        .package(url: "https://github.com/duckduckgo/content-scope-scripts", branch: "pr-releases/pr-1336"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "7.5.0"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
         .package(url: "https://github.com/duckduckgo/bloom_cpp.git", exact: "3.0.0"),
