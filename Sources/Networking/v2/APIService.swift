7bbe6d302af60435e3ece133414c72de0bb7bf49
Federico Cappelli <fcappelli@duckduckgo.com>
2025-01-24 09:16:09 +0000

    PR suggestions

--- Sources/Networking/v2/APIService.swift
+++ Sources/Networking/v2/APIService.swift
@@ -22,12 +22,12 @@ import os.log
 public protocol APIService {
     typealias AuthorizationRefresherCallback = ((_: APIRequestV2) async throws -> String)
     var authorizationRefresherCallback: AuthorizationRefresherCallback? { get set }
-    func fetch(request: APIRequestV2, authRefreshRetryCount: Int, failureRetryCount: Int) async throws -> APIResponseV2
+    func fetch(request: APIRequestV2, authAlreadyRefreshed: Bool, failureRetryCount: Int) async throws -> APIResponseV2
 }
 
 extension APIService {
     public func fetch(request: APIRequestV2) async throws -> APIResponseV2 {
-        return try await fetch(request: request, authRefreshRetryCount: 0, failureRetryCount: 0)
+        return try await fetch(request: request, authAlreadyRefreshed: false, failureRetryCount: 0)
     }
 }
 
@@ -43,7 +43,7 @@ public class DefaultAPIService: APIService {
     /// Fetch an API Request
     /// - Parameter request: A configured APIRequest
     /// - Returns: An `APIResponseV2` containing the body data and the HTTPURLResponse
-    public func fetch(request: APIRequestV2, authRefreshRetryCount: Int = 0, failureRetryCount: Int = 0) async throws -> APIResponseV2 {
+    public func fetch(request: APIRequestV2, authAlreadyRefreshed: Bool = false, failureRetryCount: Int = 0) async throws -> APIResponseV2 {
         var request = request
         
         Logger.networking.debug("Fetching: \(request.debugDescription)")
@@ -66,7 +66,7 @@ public class DefaultAPIService: APIService {
         // First time the request is executed and the response is `.unauthorized` we try to refresh the authentication token
         if responseHTTPStatus == .unauthorized,
            request.isAuthenticated == true,
-           authRefreshRetryCount == 0,
+           !authAlreadyRefreshed,
            let authorizationRefresherCallback {
 
             // Ask to refresh the token
@@ -74,7 +74,7 @@ public class DefaultAPIService: APIService {
             request.updateAuthorizationHeader(refreshedToken)
 
             // Try again
-            return try await fetch(request: request, authRefreshRetryCount: authRefreshRetryCount + 1, failureRetryCount: failureRetryCount)
+            return try await fetch(request: request, authAlreadyRefreshed: true, failureRetryCount: failureRetryCount)
         }
 
         // It's a failure and the request must be retried
@@ -88,7 +88,7 @@ public class DefaultAPIService: APIService {
             }
 
             // Try again
-            return try await fetch(request: request, authRefreshRetryCount: authRefreshRetryCount, failureRetryCount: failureRetryCount + 1)
+            return try await fetch(request: request, authAlreadyRefreshed: authAlreadyRefreshed, failureRetryCount: failureRetryCount + 1)
         }
 
         // It's not a failure, we check the constraints
