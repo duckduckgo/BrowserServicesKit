8c42dfba8ac55c8d774d5f139922db3c56ea05d2
Fernando Bunn <bunn@idevzilla.com>
2025-02-10 10:31:02 +0000

    Fix test name

--- Tests/BrowserServicesKitTests/FeatureFlagging/DefaultFeatureFlaggerTests.swift
+++ Tests/BrowserServicesKitTests/FeatureFlagging/DefaultFeatureFlaggerTests.swift
@@ -89,7 +89,7 @@ final class DefaultFeatureFlaggerTests: XCTestCase {
         XCTAssertFalse(featureFlagger.isFeatureOn(for: FeatureFlagSource.disabled))
     }
 
-    func testWhenDisabled_sourceDisabled_returnsTrue() {
+    func testWhenEnabled_sourceDisabled_returnsTrue() {
         let featureFlagger = createFeatureFlagger()
         XCTAssertTrue(featureFlagger.isFeatureOn(for: FeatureFlagSource.enabled))
     }
