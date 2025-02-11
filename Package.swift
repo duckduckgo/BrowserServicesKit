dd21660aaa08ee9eeb417b542544db61d0ab4729
dbajpeyi <3018923+dbajpeyi@users.noreply.github.com>
2025-02-11 13:15:08 +0000

    Update autofill to 16.2.1

--- Package.swift
+++ Package.swift
@@ -51,7 +51,7 @@ let package = Package(
         .library(name: "PrivacyStats", targets: ["PrivacyStats"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "16.2.0"),
+        .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "16.2.1"),
         .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.4.2"),
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit.git", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.4.0"),
