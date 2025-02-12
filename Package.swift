806b4986a15808a793508c4ff7e60551ca28d845
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-02-12 20:33:15 +0000

    C-S-S branch

--- Package.swift
+++ Package.swift
@@ -56,7 +56,7 @@ let package = Package(
         .package(url: "https://github.com/duckduckgo/TrackerRadarKit.git", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.4.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "3.0.0"),
-        .package(url: "https://github.com/duckduckgo/content-scope-scripts", exact: "7.18.0"),
+        .package(url: "https://github.com/duckduckgo/content-scope-scripts", branch: "pr-releases/pr-1421"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "8.4.0"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
         .package(url: "https://github.com/duckduckgo/bloom_cpp.git", exact: "3.0.0"),
